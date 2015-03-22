b56670c19ccaf6be4adc01a2b81bae9752a4eee9
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-03-22 09:00:45 -0700

    Create 026_remove_duplicates_sorted_array.cc

---
+++ 026_remove_duplicates_sorted_array.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  int removeDuplicates(int A[], int n) {
+    if(n == 0) return 0;
+  	int i(0), j(1), prev = INT_MAX;
+	   for(j = 0; j < n; ++j) {
+	    if(A[j] != prev) {
+	      A[i++] = A[j]; prev = A[j];
+	    }
+	  }
+	  return i;   
+  }
+};
