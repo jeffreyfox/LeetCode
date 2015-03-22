58ae39d13e8caeb5037591dc9568bb2c8a78bcac
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-03-22 08:56:55 -0700

    Update 001_two_sum.cc

--- 001_two_sum.cc
+++ 001_two_sum.cc
@@ -2,19 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
         map<int, int> s; //occurrence index
-        vector<int> ret;
+        vector<int> ret(2, 0);
         int n = numbers.size();
         if(n == 0) return ret;
-        vector<int> numbers_rec(n);
+        //build hash table first
+        for(int k = 0; k < n; ++k)  s[numbers[k]] = k;
+        //another sweep
         for(int k = 0; k < n; ++k) { 
-            s[numbers[k]] = k;
-        }
-        for(int k = 0; k < n; ++k) { 
-            int val = target - numbers[k];
-            map<int, int>::iterator it = s.find(val);
-            if(it != s.end() && it->second != k) { //not the same entry
-                ret.push_back(k+1);
-                ret.push_back(it->second+1);
+            //find residue in map
+            map<int, int>::iterator it = s.find(target-numbers[k]);
+            if(it != s.end() && it->second != k) { //found and not the same entry
+                ret[0] = k+1;  ret[1] = it->second+1;
                 break;
             }
         }
