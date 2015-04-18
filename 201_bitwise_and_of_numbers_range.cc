5a646970b1341a58348f826eb213a83ad328e36d
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-04-18 13:34:15 -0700

    Update 201_bitwise_and_of_numbers_range.cc

--- 201_bitwise_and_of_numbers_range.cc
+++ 201_bitwise_and_of_numbers_range.cc
@@ -1,4 +1,8 @@
-/* If we search from highest bits to lowest bits, the result should have the same bit if m and n have the same bit, and once we encountered the first different bit, then all the bits in result on and after this bit should be zero. The reason is as follows. Suppose the first different bit is at i, then m[i] should be 0 and n[i] should be 1 (m <=n), then the numbers ..011111 and ..100000 should be between m and n, and it will give all zeros after AND operations. Thus, the key is to find the highest set bit of m^n, and use ~(m^n-1) as the mask.
+/* If we search from highest bits to lowest bits, the result should have the same bit if m and n have the same bit, 
+and once we encountered the first different bit, then all the bits in result on and after this bit should be zero. 
+The reason is as follows. Suppose the first different bit is at i, then m[i] should be 0 and n[i] should be 1 (m <=n), 
+then the numbers ..011111 and ..100000 should be between m and n, and it will give all zeros after AND operations. 
+Thus, the key is to find the highest set bit of m^n, and use ~(m^n-1) as the mask.
 Also works for m == n case, where m_and = m, and m_xor = 0, the return value is thus m & ~0 = m & 1111111 = m
 */
 class Solution {
