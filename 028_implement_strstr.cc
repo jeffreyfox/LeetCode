020921b5ab24e0300c82c332df9b9b99afe70f1b
jeffreyfox <jeffreyfox@users.noreply.github.com>
2015-05-09 19:55:01 -0700

    Create 028_implement_strstr.cc

---
+++ 028_implement_strstr.cc
@@ -0,0 +1,17 @@
+/// Classical Pattern search. Brute-force solution, O(MN) in the worst case
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size(); // text
+        int m = needle.size(); // pattern
+        for(int i = 0; i <= n-m; ++i) {
+            int j;
+            for(j = 0; j < m; ++j) {
+                if(haystack[i+j] != needle[j]) break;
+            }
+            if(j == m) return i;
+        }
+        return -1;
+    }
+};
