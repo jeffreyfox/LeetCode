2cb6892ee764f7e38d1d38e00afe096a8193a603
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-07-10 20:54:07 -0700

    Add 065_valid_number.cc with comments

--- 065_valid_number.cc
+++ 065_valid_number.cc
@@ -1,19 +1,89 @@
-/* Use finite automaton. The number consists of several fields: 
- *
- * Number text :    -28.82e-20   
- * Field number: 0001223445677888
+/*
+ * A valid number consists of several fields:
+ *    -28.82e-20 
+ * 00012222233334444  
 
- * field 0 is the blank character, can appear >=0 times, \s*
- * field 1 is either plus or minus sign, and can appear zero or one times, [\+\-]?
- * field 2 is the integer part, if exist, can have >=1 numbers, can also be absent if field 3 (dot) is present. \d+ | \.
- * field 3 is the dot, can appear once or be absent (pure integer) \.?
- * fields 2,3,4, we can only have 2, (e.g. 28), or 2-3 (28.0), or 2-3-4 (28.02), or 3-4 (.02), thus \d+ (\.\d*)? | \. \d+ (left of | is with 2, right of | is without 2)
- * field 4 is the fractional part, can be absent or contain multiple integers \d*
- * field 5 is the e/E character [eE]
- * field 6 is the sign, [\+\-]?
- * field 7 is the exponent, only integer is allowed, \d+.
- * fields 5,6,7 can be absent, thus ([eE][+-]?\d+)?
- * field 8 is the blank character, can appear >=0 times, \s*
+ * Fields 0 and 4 are spaces and can appear >=0 times, thus: \s*
+ * Field 1 is either plus or minus sign, and can appear zero or one times, thus:  [\+\-]?
+ * Field 2 is the coefficient part, can be one of the following:
+ * - integers (a sequence of digits) 20348
+ * - integers plus a dot: 20345.
+ * - floating points: 2.304
+ * - dot followed by digits: .234
+ * It should not accept a single dot (.). 
+ * Depending on whether a dot is the first character we can break it into two cases:
+ * - first character is a digit, so the dot and fractional points can be absent, thus: \d+(\.\d*)?
+ * - second character is a dot, it has to be followed by at least one digit, thus: \.\d+
+ * thus field 2 is: \d+ (\.\d*)? | \.\d+ 
+ * Field 3 is the exponential part. It consists of several parts: 
+ * - the e/EXPO character: [eE]
+ * - the sign (either + or - or missing): [\+\-]?
+ * - the exponent as a sequence of numbers (at least one): \d+.
+ * field 3 can be absent or appear only one time, thus: ([eE][\+\-]?\d+)?
  * Thus the regular expression for the valid number is:
- * \s*[\+\-]?(\d+(\.\d*)?|\.\d+)([eE][\+\-]?\d+)?\s*
+ *  \s* [\+\-]? (\d+(\.\d*)?|\.\d+) ([eE][\+\-]?\d+)? \s*  (spaces in between are added for clarity and should be removed)
+ * 0..0 1  .. 1 2      ..         2 3     ..        3 4..4
  */
+
+// Use finite automaton. Read charater one by one, depending on the current state and character read, jump to the next state
+// 9 states,  and 6 types of charaters defined in CharType
+// Small trick: add a space after string s, so we only need to check if state == 8 in the end. 
+// Alternative: not add a space, and instead check if state is one of 2,4,7,8 in the end.
+class Solution {
+public:
+    enum CharType { SPACE=0, DIGIT, DOT, SIGN, EXPO, OTHER };
+
+    bool isNumber(string s) {
+        s += ' ';
+        initDFA();
+        int state = 0;
+        for (int i = 0; i < s.size() && state >= 0; ++i)
+            state = dfa[state][getType(s[i])];
+        
+        return state == 8;
+    }
+
+    void initDFA() {
+        dfa.resize(9, vector<int>(6, -1));
+        // state 0: has only seen spaces so far, expects another space, a digit, a dot, or a sign
+        dfa[0][SPACE] = 0;
+        dfa[0][DIGIT] = 2;
+        dfa[0][DOT]   = 3;
+        dfa[0][SIGN]  = 1;
+        // state 1: has read a sign (+/-), expects a digit, or a dot
+        dfa[1][DIGIT] = 2;
+        dfa[1][DOT]   = 3;
+        // state 2: has read a digit in coeffient part, expects another digit, a dot, a e/E, or space
+        dfa[2][SPACE] = 8;
+        dfa[2][DIGIT] = 2;
+        dfa[2][DOT]   = 4;
+        dfa[2][EXPO]  = 5;
+        // state 3: has read a dot (.) before reading any digit, only expects a digit as fractional part only
+        dfa[3][DIGIT] = 4;
+        // state 4: has read a dot (.) after reading at least one digit, expects a digit, or e/E, or space
+        dfa[4][SPACE] = 8;
+        dfa[4][DIGIT] = 4;
+        dfa[4][EXPO]  = 5;
+        // state 5: has read a e/E in exponent field, expects a sign, or a digit
+        dfa[5][DIGIT] = 7;
+        dfa[5][SIGN]  = 6;
+        // state 6: has read a sign in exponent field, expects a digit
+        dfa[6][DIGIT] = 7;
+        // state 7: has read a digit in exponent field, expects another digit, or space
+        dfa[7][SPACE] = 8;
+        dfa[7][DIGIT] = 7;
+        // state 8: has read a trailing space, expects another space
+        dfa[8][SPACE] = 8; 
+    }
+
+    int getType(char c) {
+        if (c == ' ' || c == '\t') return SPACE;
+        if (c >= '0' && c <= '9')  return DIGIT;
+        if (c == '.')              return DOT;
+        if (c == '+' || c == '-')  return SIGN;
+        if (c == 'e' || c == 'E')  return EXPO;
+        return OTHER;
+    }
+
+    vector<vector<int> > dfa;
+};
