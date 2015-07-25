77f5deb9c0d13e7896cbb12df4ace373a6d3f6ee
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-07-25 14:47:14 -0700

    Add 020_valid_parentheses.cc

---
+++ Algorithms/020_valid_parentheses.cc
@@ -0,0 +1,29 @@
+// Use a stack. When seeing a left parenthesis, push to stack; otherwise check against top of stack
+// After reaching the end of string, need to check if stack is empty! (e.g. '[' is not valid)
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            char c = s[i];
+            if(isLeft(c)) st.push(c);
+            else if (st.empty() || !match(st.top(), c)) return false; //is right, no match
+            else st.pop(); //match, pop out
+        }
+        return st.empty();
+    }
+    
+    bool isLeft(char c) {
+        return c == '(' || c == '[' || c == '{';
+    }
+    bool isRight(char c) {
+        return c == '(' || c == '[' || c == '{';
+    }
+    bool match(char c1, char c2) {
+        return (c1 == '(' && c2 == ')') ||
+                (c1 == '[' && c2 == ']') ||
+                (c1 == '{' && c2 == '}');
+    }
+};
