4b8020d5aae7fa6094093a2fa9a987a475bd0d62
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-08-07 21:18:47 -0700

    Add 093_restore_ip_addresses.cc

---
+++ Algorithms/093_restore_ip_addresses.cc
@@ -0,0 +1,41 @@
+// recursive solution.
+// Need to add 4 periods, last one has to be after last character.
+// store positions of periods (index of character right before newly added period), and reconstruct the string when valid solutions are found
+// Caveat: string::insert function argument is the index which the new character is inserted before.
+// Another caveat: ip address segments cannot have leading zeros, except .0. case, others like .03. or .00. are illegal. Need to filter them out
+
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> ret;
+        vector<int> dotPos(4, -1); //dot positions in the s
+        restore(s, 0, 0, dotPos, ret);
+        return ret;
+    }
+    //checking substring starting at s[i] for dot number k (0-3), add values to dotPos
+    void restore(const string& s, int i, int k, vector<int>& dotPos, vector<string>& ret) {
+       if (k == 4) { //already placing 4 dots (one at the end)
+          //onlyd add when i reaches the end
+          if (i == s.size()) ret.push_back(construct(s, dotPos));
+          return;
+       }
+       if (i == s.size()) return;
+       int v = 0;
+       for (int j = i; j < s.size(); ++j) {
+          v = 10*v + s[j] - '0';
+          if(v >= 256) break;
+          dotPos[k] = j; //dot is after j's position
+          restore(s, j+1, k+1, dotPos, ret);
+          if(s[i] == '0') break; //if leading character is '0', do not continue loop ('.00.' is illegal IP segment)
+       }
+    }
+    string construct(const string& s, const vector<int>& dotPos) {
+        string ret = s;
+        ret.insert(dotPos[2]+1, 1, '.');
+        ret.insert(dotPos[1]+1, 1, '.');
+        ret.insert(dotPos[0]+1, 1, '.');
+        return ret;
+    }
+
+};
+
