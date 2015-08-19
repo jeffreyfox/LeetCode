f6f35d777d1d6f252ebd5cf8ba9db8f1d4fe39dd
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-08-19 09:14:07 -0700

    Add 035_search_insert_position.cc

---
+++ Algorithms/035_search_insert_position.cc
@@ -0,0 +1,30 @@
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+*/
+
+// Binary search. If found, return index
+// If not found, return position lo, where A[0, .. lo-1] strictly smaller than target, and A[lo .. n] > target
+
+class Solution {
+    
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int lo(0), hi(n-1), mid(0);
+        while(lo <= hi) {
+            mid = lo + (hi-lo)/2;
+            if(target == nums[mid]) return mid;
+            if(target < nums[mid]) hi = mid-1;
+            else lo = mid+1;
+        }
+        return lo;
+    }
+}; 
