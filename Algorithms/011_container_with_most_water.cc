eb6a93bd51c172a5aae6f0a135954b95f5604491
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-08-19 09:04:23 -0700

    Add 011_container_with_most_water.cc

---
+++ Algorithms/011_container_with_most_water.cc
@@ -0,0 +1,22 @@
+/*Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container. 
+*/
+
+//Similar idea to "trapping rain water". Maintain two pointers and find the minimum water level deterimed by the two pointers. Move from the side with lower height, stop when two pointers meet
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int area = 0;
+        int n = height.size();
+        if(n <= 1) return 0;
+        int l(0), r(n-1);
+        while(l < r) {
+            area = max(area, min(height[l], height[r])*(r-l));
+            if(height[l] < height[r]) l++;
+            else r--;
+        }
+        return area;
+    }
+};
