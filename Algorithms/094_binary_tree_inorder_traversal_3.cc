86cbf5c9156942e4171486926a4cf4bdc184064b
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-08-21 09:13:21 -0700

    Add 094_binary_tree_inorder_traversal_3.cc

---
+++ Algorithms/094_binary_tree_inorder_traversal_3.cc
@@ -0,0 +1,59 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+
+   1
+    \
+     2
+    /
+   3
+
+return [1,3,2].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// Iterative solution using a stack (and a tag indicating the number of times an element appears in stack).
+
+// Slightly different version with modified control flow with tag.
+
+class Solution {
+public:
+    struct Element {
+        TreeNode *node;
+        int tag;
+        Element(TreeNode *n, int t) : node(n), tag(t) {}
+        Element() : node(NULL), tag(-1) {}
+    };
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ret;
+        if(!root) return ret;
+        deque<Element> s; //stack
+        s.push_back(Element(root, 0));
+        while(!s.empty()) {
+            TreeNode *node = s.back().node;
+            int tag = s.back().tag;
+            s.pop_back();
+            if(tag == 0) { //first time seen, process left subtrees
+                s.push_back(Element(node, 1));
+                if(node->left) s.push_back(Element(node->left, 0));
+            } else { //second time seen, add to result, and process right
+                ret.push_back(node->val);
+                if(node->right) s.push_back(Element(node->right, 0));
+            }
+        }
+        return ret;
+    }
+};
