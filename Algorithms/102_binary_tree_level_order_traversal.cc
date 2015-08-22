f2e030ac0584b081efd9717051990ff3d89e713c
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-08-22 09:39:33 -0700

    Add 102_binary_tree_level_order_traversal.cc

---
+++ Algorithms/102_binary_tree_level_order_traversal.cc
@@ -0,0 +1,58 @@
+/*
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree {3,9,20,#,#,15,7},
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its level order traversal as:
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+*/
+
+/// Classical solution using a queue
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    struct Element {
+        TreeNode *node;
+        int level;
+        Element(TreeNode *n, int l) : node(n), level(l) {}
+    };
+    
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int> > ret;
+        if(!root) return ret;
+        deque<Element> q; //queue
+        q.push_back(Element(root, 0));
+        while(!q.empty()) {
+            TreeNode *node = q.front().node;
+            int level = q.front().level;
+            q.pop_front();
+            if(level == ret.size()) ret.push_back(vector<int>());
+            ret[level].push_back(node->val);
+            if(node->left) q.push_back(Element(node->left, level+1));
+            if(node->right) q.push_back(Element(node->right, level+1));
+        }
+        return ret;
+    }
+};
