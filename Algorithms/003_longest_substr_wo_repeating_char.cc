d1dc12a2d957c301c7cd5ea2a3c7e1c761b06acd
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-14 10:10:31 -0800

    003_longest_substr_wo_repeating_char

--- Algorithms/003_longest_substr_wo_repeating_char.cc
+++ Algorithms/003_longest_substr_wo_repeating_char.cc
@@ -2,7 +2,7 @@
 Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
 */
 
-// Solution 1, use a map (slow). Scan from left to right, and maintain a map to store characters already seen and its last position, update length each time.
+// Solution 1, use a map to store the position of each character (since s is string, can use an indexed array which is faster than map). Scan from left to right, and maintain a map to store characters already seen and its last position, update length each time.
 // Use a pointer (i) to store the current front of substring
 // When a character is not in the map, insert it to map; 
 // When a character is found and its index is within i and j, then move i to one character to the right of it. 
@@ -10,13 +10,11 @@ Given a string, find the length of the longest substring without repeating chara
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        map<char, int> pos;
+        vector<int> pos(256, -1); //map<char, int> pos;
         int max_len(0);
         for(int i = 0, j = 0; j < s.size(); ++j) {
             char c = s[j];
-            if(pos.count(c) > 0 && pos[c] >= i) {
-                 i = pos[c]+1;
-            }
+            if(pos[c] >= i)  i = pos[c]+1;
             pos[c] = j;
             max_len = max(max_len, j-i+1);
         }
--- Algorithms/003_longest_substr_wo_repeating_char_2.cc
+++ Algorithms/003_longest_substr_wo_repeating_char_2.cc
@@ -2,19 +2,22 @@
 Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
 */
 
-// Solution 2, use an indexed array lookup table instead of map for better efficiency.
-
+// Another solution by counting the occurrences of each character, similar to problem 159.
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> pos(256, -1); //map<char, int> pos;
-        int max_len(0);
-        for(int i = 0, j = 0; j < s.size(); ++j) {
-            char c = s[j];
-            if(pos[c] >= i)  i = pos[c]+1;
-            pos[c] = j;
-            max_len = max(max_len, j-i+1);
-        }
-        return max_len;
+         int n = s.size(); //number of records
+         int istart = 0, max_len = 0;
+         int count = 0;
+         vector<int> dict(256);
+         for(int i = 0; i < n; ++i) {
+            char c = s[i];
+            ++dict[c];
+            while(dict[c] > 1) {
+               --dict[s[istart++]];
+            }
+            max_len = max(max_len, i-istart+1);
+         }
+         return max_len;
     }
 };
