d07490834763e63545d820c7f42e3e44e4860846
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-16 22:07:13 -0800

    update

--- Algorithms/015_3sum.cc
+++ Algorithms/015_3sum.cc
@@ -1,5 +1,21 @@
+/*
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+    Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+    The solution set must not contain duplicate triplets.
+
+    For example, given array S = {-1 0 1 2 -1 -4},
+
+    A solution set is:
+    (-1, 0, 1)
+    (-1, -1, 2)
+*/
+
 /// O(n2) solution
-/// Need to consider duplicated entries. See lines 14 and 27.
+/// Need to consider duplicated entries.
+/// Optimization: break if nums[i] > 0, and also num[n-1] < 0 (52ms => 48ms)
 
 class Solution {
 public:
@@ -7,25 +23,27 @@ public:
         vector<vector<int> > ret;
         int n = nums.size();
         if(n <= 2) return ret;
-        vector<int> sol(3, 0);
+        vector<int> tmp(3, -1);
         sort(nums.begin(), nums.end());
-        
-        for(int i = 0; i < n; ++i) { //whether nums[i] is one of the triplet?
-            if(i > 0 && nums[i] == nums[i-1]) continue; //ignore duplicates
-            
-            int res = -nums[i];
-            int lo = i+1, hi = n-1; //linear search for the other two
-           
-            while(lo < hi) {
-                
-                if(nums[lo] + nums[hi] > res) hi--;
-                else if(nums[lo] + nums[hi] < res) lo++;
-                else { //equal
-                    sol[0] = nums[i], sol[1] = nums[lo], sol[2] = nums[hi];
-                    ret.push_back(sol);
-                    hi--; //just decrement hi to search next (can also increment lo)
-                    while(lo < hi && nums[hi] == nums[hi+1]) hi--; //avoid duplicates
-                }
+        if(nums[n-1] < 0) return ret; //no need to continue if largest value is < 0
+        for(int i = 0; i < n-2; i++) {
+            if(nums[i] > 0) break; //no need to continue if found a positive number
+            if(i > 0 && nums[i] == nums[i-1]) continue; //avoid duplicates
+            if(nums[i] + nums[i+1] + nums[i+2] > 0) break;
+            if(nums[i] + nums[n-2] + nums[n-1] < 0) continue;
+            int target = -nums[i];
+            int j = i+1, k = n-1;
+            while(j < k) {
+               int sum = nums[j] + nums[k];
+               if(sum < target) j++;
+               else if(sum > target) k--;
+               else {
+                   tmp[0] = nums[i]; tmp[1] = nums[j]; tmp[2] = nums[k];
+                   ret.push_back(tmp);
+                   j++; k--;
+                   while(j < k && nums[j] == nums[j-1]) j++; //avoid duplicates
+                   while(j < k && nums[k] == nums[k+1]) k--; //avoid duplicates
+               }
             }
         }
         return ret;
--- Algorithms/016_3sum_closest.cc
+++ Algorithms/016_3sum_closest.cc
@@ -1,25 +1,36 @@
+/*
+Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+    For example, given array S = {-1 2 1 -4}, and target = 1.
+
+    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+*/
+
 /// O(n2) solution
-/// When searching the next two in linear time (as in 2-sum cases), has to scan all elements, can't break in the middle when diff changes sign!
 
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        
+        int result = 0;
+        int min_diff = INT_MAX;
         int n = nums.size();
-        int ret = 0;
-        int diff = INT_MAX, min_diff = INT_MAX;
-        for (int i = 0; i < n-2; ++i) {
-            //find the next two in linear time
+        if(n <= 2) return 0;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n-2; ++i) {
+            if(i > 1 && nums[i] == nums[i-1]) continue;
             int j = i+1, k = n-1;
             while(j < k) {
-                diff = target - (nums[i] + nums[j] + nums[k]);
-                if (diff > 0) j++;
-                else if (diff < 0) k--;
-                else return target;
-                if (abs(diff) < abs(min_diff)) min_diff = diff;
+                int sum = nums[i] + nums[j] + nums[k];
+                int diff = target - sum;
+                if(diff == 0) return target;
+                else if(diff < 0) k--;
+                else j++;
+                if(abs(diff) < min_diff) {
+                    min_diff = abs(diff);
+                    result = sum;
+                }
             }
         }
-        return target - min_diff;
+        return result;
     }
 };
