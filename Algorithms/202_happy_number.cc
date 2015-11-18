e46c4eb62b0de576c9b1e732328b56227c607e68
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-17 21:18:01 -0800

    202_happy_number

--- Algorithms/202_happy_number.cc
+++ Algorithms/202_happy_number.cc
@@ -1,25 +1,39 @@
+/*
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 19 is a happy number
+
+    12 + 92 = 82
+    82 + 22 = 68
+    62 + 82 = 100
+    12 + 02 + 02 = 1
+*/
+
 /// Use a hash map (set) to record all numbers and check if we encounter a previous one (loop).
 /// If found a "1" then is a happy number, otherwise not.
 class Solution {
 public:
     bool isHappy(int n) {
-        set<int> occur;
-        occur.insert(n);
-        while(1) {
-            n = next(n);
-            if(n == 1) return true;
-            if(occur.find(n) != occur.end()) return false;
-            occur.insert(n);
+        if(n == 0) return false;
+        if(n == 1) return true;
+        unordered_set<int> dict;
+        dict.insert(1);
+        while(dict.count(n) == 0) {
+            dict.insert(n);
+            n = calc(n);
         }
+        return n == 1;
     }
     
-    int next(int n) {
-        int sum = 0;
-        while(n > 0) {
-            int res = n % 10;
-            sum += res*res;
-            n = n / 10;
+    int calc(int n) {
+        int result = 0;
+        while(n) {
+            int r = n % 10;
+            result += r*r;
+            n /= 10;
         }
-        return sum;
+        return result;
     }
 };
