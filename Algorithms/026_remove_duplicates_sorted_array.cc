54aea8af2ba64b2cf7214c9802578bb780fda2e3
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-18 09:11:25 -0800

    update

--- Algorithms/026_remove_duplicates_sorted_array.cc
+++ Algorithms/026_remove_duplicates_sorted_array.cc
@@ -1,13 +1,44 @@
+/*
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length. 
+*/
+
+// Two pointers
 class Solution {
 public:
-	int removeDuplicates(int A[], int n) {
-		if(n == 0) return 0;
-		int i(0), j(1), prev = INT_MAX;
-		for(j = 0; j < n; ++j) {
-			if(A[j] != prev) {
-				A[i++] = A[j]; prev = A[j];
-			}
-		}
-		return i;   
-	}
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 1) return n;
+        int len = 0;
+        int i = 0, j = 0;
+        while(i < n) {
+            j = i+1;
+            while(j < n && nums[j] == nums[i]) j++;
+            nums[len++] = nums[i];
+            i = j;
+        }
+        return len;
+    }
+};
+
+// A slightly different solution (no len variable)
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 1) return n;
+        int i = 0, j = 1;
+        while(1) {
+            while(j < n && nums[j] == nums[i]) j++;
+            if(j == n) break;
+            nums[++i] = nums[j++];
+        }
+        return i+1;
+    }
 };
--- Algorithms/080_remove_duplicates_sorted_array_ii.cc
+++ Algorithms/080_remove_duplicates_sorted_array_ii.cc
@@ -1,3 +1,33 @@
+/*
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+
+For example,
+Given sorted array nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length. 
+*/
+
+// Solution: two pointers. 
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 1) return n;
+        int len = 0;
+        int i = 0, j = 0;
+        while(i < n) {
+            j = i+1;
+            while(j < n && nums[j] == nums[i]) j++;
+            if(j >= i+1) nums[len++] = nums[i];
+            if(j > i+1) nums[len++] = nums[i];
+            i = j;
+        }
+        return len;
+    }
+};
+
+// A old solution
 class Solution {
 public:
     int removeDuplicates(int A[], int n) {
