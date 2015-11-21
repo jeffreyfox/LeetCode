3815cd2ac4389cb64a561b38f150c2c859be40ec
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-21 15:35:49 -0800

    001_two_sum

--- Algorithms/001_two_sum.cc
+++ Algorithms/001_two_sum.cc
@@ -33,3 +33,65 @@ public:
         return ret;
     }
 };
+
+// This solution uses a map to store the positions of each number in array. Only one pass is needed.
+// For each number in the array, see if the residue (target - number) already exists in the map, if yes then found; if no, then add it to map
+// This also properly handles duplicates, because checking residue happens before insertion. 
+// Unordered map (hash table) is faster than map (which is balanced tree) in this case.
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      unordered_map<int, int> table;
+      vector<int> ret(2, -1);
+      int n = nums.size();
+      for(int i = 0; i < nums.size(); i++) {
+        int res = target - nums[i];
+        if(table.count(res)) {
+           ret[0] = table[res]+1; ret[1] = i+1;
+           break;
+        } else {
+           table[nums[i]] = i;
+        }
+      }
+      return ret;
+    }
+};
+
+// This version uses insertion sort t sort nums, together with its positions as satellite data. Use two pointers to find the 
+// target items, and return their positions. (No hash table is involved)
+// Insertion sort: O(n2), search for target: O(n).
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> pos(n);
+        for(int i = 0; i < n; ++i) pos[i] = i+1;
+        //insertion sort nums
+        for(int i = 1; i < n; ++i) {
+            int tmp = nums[i];
+            int tmp2 = pos[i];
+            int j = i-1;
+            while(j >= 0 && nums[j] > tmp) {
+                nums[j+1] = nums[j]; 
+                pos[j+1] = pos[j];
+                j--;
+            }
+            nums[j+1] = tmp;
+            pos[j+1] = tmp2;
+        }
+        
+        int l = 0, r = nums.size()-1;
+        vector<int> ret(2, 0);
+        while(1) {
+            int sum = nums[l] + nums[r];
+            if(target == sum) {
+                ret[0] = min(pos[l], pos[r]), ret[1] = max(pos[l], pos[r]);
+                return ret;
+            } else if (target > sum) l++;
+            else r--;
+        }
+        return ret;
+    }
+};
+
