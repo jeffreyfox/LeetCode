0f9bf18600118e32446494c303847ac64fb67142
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-21 16:54:43 -0800

    149_max_points

--- Algorithms/149_max_points_on_a_line.cc
+++ Algorithms/149_max_points_on_a_line.cc
@@ -12,36 +12,72 @@ Given n points on a 2D plane, find the maximum number of points that lie on the 
  * };
  */
 
-/// Iterate all points. For each point i, check all points (including i itself). There are three cases:
-/// 1. duplicate point as i, track count as n_dup. n_dup is at least one
+/// O(n2) solution with hash table.
+/// Iterate all points. For each point i, check all points after it (excluding itself). There are three cases:
+/// 1. duplicate point as i, track count as n_dup.
 /// 2. lying along same vertical line as point i, track count as n_ver
-/// 3. lying on a line of a finite slope. maintain a map of slope and count. Track the maximum of counts of all slopes as n_slp.
-/// After scanning all other points, we have n_dup, n_ver, and n_slp. Then the maximum number of points lying across i is: max(n_ver, n_slp) + n_dup.
+/// 3. lying on a line of a finite slope. maintain a map of slope and count. Track the maximum of counts of all slopes as max_line.
+/// After scanning all other points, we have num_dup, num_ver, and max_line. Then the maximum number of points lying across i is: 1 + num_dup + max(max_line, num_ver), where 1 counts point i itself.
 /// repeat for all points and update
 /// Caveat: when calculating the slope from integer coordinates, convert the numerator to double!
 
 class Solution {
 public:
     int maxPoints(vector<Point>& points) {
-        if(points.empty()) return 0;
-        int maxp = 0; //at least one
-        map<double, int> slopes;
         int n = points.size();
-        for(int i = 0; i < n; ++i) {
-            slopes.clear();
-            int n_dup(0), n_ver(0);
-            int n_slp(0);
-            for(int j = 0; j < n; ++j) {
-                if(points[i].x == points[j].x) {
-                    if(points[i].y == points[j].y) n_dup++;
-                    else n_ver++;
-                } else {
-                    double sl = double(points[j].y - points[i].y) / (points[j].x - points[i].x);
-                    n_slp = max(n_slp, ++slopes[sl]);
+        if(n <= 2) return n;
+        int result = 0;
+        unordered_map<double, int> dict;
+        for(int i = 0; i < n-1; ++i) { //no need to scan last point
+            int num_dup = 0, num_ver = 0, max_line = 0; //# duplicates, # points on same vertical line, max # points on any line
+            dict.clear();
+            //check all the points from i+1 to n
+            for(int j = i+1; j < n; ++j) {
+                if(points[j].x == points[i].x) {
+                    if(points[j].y == points[i].y) num_dup++; //same point
+                    else num_ver++; //on same vertical line
+                } else { //calculate slope
+                    double slope = double(points[j].y - points[i].y) / (points[j].x - points[i].x);
+                    max_line = max(max_line, ++dict[slope]);
                 }
             }
-            maxp = max(maxp, max(n_slp, n_ver) + n_dup);
+            result = max(result, 1 + num_dup + max(max_line, num_ver));
         }
-        return maxp;
+        return result;
     }
 };
+
+
+/// O(n3) solution
+/// Check each pair (pi, pj) and count all points lying on the line determined by pi, pj (using colinear function).
+// If encountered duplicates, count them as well
+
+class Solution {
+public:
+    int maxPoints(vector<Point>& points) {
+        int n = points.size();
+        if(n <= 2) return n;
+        int result = 0;
+        for(int i = 0; i < n-1; ++i) {
+            int num_dup = 1, num_col = 0;
+            for(int j = i+1; j < n; ++j) {
+                if(points[i].x == points[j].x && points[i].y == points[j].y) {
+                    num_dup++;
+                    continue;
+                }
+                num_col = 1; //count point[j]
+                for(int k = j+1; k < n; ++k) {
+                    if(colinear(points[i], points[j], points[k])) num_col ++;
+                }
+                result = max(result, num_dup + num_col);
+            }
+            result = max(result, num_dup); //deal with case where only exist duplicates, no colinear execution
+        }
+        return result;
+    }
+    
+    bool colinear(Point& p, Point& q, Point& r){
+        return (q.y - p.y)*(r.x - q.x) - (r.y - q.y)*(q.x - p.x) == 0;
+    }
+};
+
