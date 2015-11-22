7ceadfb43245b7658ba1e344409ef97cfc825fcf
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-21 20:18:16 -0800

    004_median_of_two_sorted_arrays

--- Algorithms/004_median_of_two_sorted_arrays.cc
+++ Algorithms/004_median_of_two_sorted_arrays.cc
@@ -1,46 +1,88 @@
+/*
+There are two sorted arrays nums1 and nums2 of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+*/
+
+/*
+Solution 1: compare medians of the two array.
+We make a general algorithm that returns the k-th order value from two sorted arrays ( 0 <= k < m+n ). Then use it to calculate the median.
+First some mathematics:
+Generally, for an array nums[lo, hi], with length len = hi-lo+1, mid = (lo+hi)/2 = lo + ceil((hi-lo)/2.0). Let hlen = (hi-lo)/2. Thus, number of elements before mid (exclusive) is ceil((hi-lo)/2.0) = hlen, and number of elements after mid (exclusive) is floor((hi-lo)/2.0) which can be either hlen or hlen+1, depending on the length of array. Here we have used the fact that
+for any integer: ceil(x/2.0) + floor(x/2.0) = x
+
+Now we have two arrays nums1[lo1, hi1], nums2[lo2, hi2], we calculate mid1 and mid2 accordingly. We compare nums1[mid1] and nums2[mid2], say we have nums1[mid1] <= nums2[mid2]. If we combine the two in sorted order we will have something like:
+      ... nums1[mid1] ... nums2[mid2] ...
+From above, we know that:
+a. nums1[mid1] has at least hlen1 + hlen2 + 1 (from nums1, nums2, and nums2[mid2] respectively) elements behind it (exlucsively)
+b. nums2[mid2] has at least hlen1 + hlen2 + 1 elements ahead of it (exclusively)
+
+Now let's check k's value, the k-th element (k starts from 0) has k elements before it,
+1. if k <= hlen1 + hlen2, then the element can have at most hlen1+hlen2 elements ahead of it. Then we know for sure that the answer is before nums2[mid2], so we can discard nums2[mid2] and the elements in nums2 after it can be excluded
+2. if k > hlen1 + hlen2, then the element can have at least hlen1+hlen2+1 elements ahead of it. Can we discard nums1[mid1] and the ones in nums1 ahead of it? Yes! Because nums1[mid1] will have 
+at most hlen1 + hlen2 elements ahead of it, including nums1[mid1] we have hlen1+hlen2+1, which we can safely exclude mathemetically (in practice we only exclude elements nums1[0 .. mid1]). The calculation is below:
+   number of elements before and including nums1[mid1] <= len1 + len2 - (hlen1 + hlen2 + 1)
+                                                        = (hi1-lo1+1) + (hi2-lo2+1) - ( floor((hi1-lo1)/2.0) + floor((hi2-lo2)/2.0 + 1)
+                                                        = ceil((hi1-lo1)/2.0) + ceil((hi2-lo2)/2.0) + 1
+                                                        = hlen1 + hlen2 + 1
+Similar for the case where nums1[mid1] > nums2[mid2]
+*/
+
 class Solution {
 public:
-	double findMedianSortedArrays(int A[], int m, int B[], int n) {
-		int k = (m+n)/2; 
-		if((m+n)%2 == 0) { //even
-			int a = Select(A, m, B, n, k);
-			int b = Select(A, m, B, n, k+1);
-			return double(a+b)/2.0;
-		} else return Select(A, m, B, n, k+1); //odd
-	}
-
-	//Binary search algorithm to selelect k-th order statistics from two SORTED arrays. Average O(log(m+n)) time.
-	int Select(int A[], int m, int B[], int n, int k) {
-		int idx = SelectUtil(A, B, n, 0, m-1, k); //search in A
-		if(m == 0) return B[k-1];
-		else if(n == 0) return A[k-1];
-		
-		if(idx != -1) return A[idx];
-		else {
-			idx = SelectUtil(B, A, m, 0, n-1, k); //search in B
-			return B[idx];
-		}
-	}
-    
- 	//Utility function to find k-th order static in A+B in A[l .. r], B's size is n
-	int SelectUtil(int A[], int B[], int n, int l, int r, int k) {
-		if(r < l) return -1;
-		int m = (l+r)/2;
-		int x = A[m];
-		//x is <= m+1 elements in A, check if x is <= k-(m+1) elements in B
-		int q = k-m-1;
-		if(q < 0) return SelectUtil(A, B, n, l, m-1, k); //search left (A[l .. m-1])
-		else if(q > n) return SelectUtil(A, B, n, m+1, r, k); //search right (A[m+1 .. r])
-		else if(q == 0) {
-			if(x <= B[q]) return m;
-			else return SelectUtil(A, B, n, l, m-1, k);
-		} else if (q == n) {
-			if( x >= B[q-1]) return m;
-			else return SelectUtil(A, B, n, m+1, r, k);
-		} else {
-			if(x >= B[q-1] && x <= B[q]) return m;
-			else if (x > B[q]) return SelectUtil(A, B, n, l, m-1, k);
-			else return SelectUtil(A, B, n, m+1, r, k);
-		}
-	}
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        int k = (m+n)/2;
+        if((m + n) % 2 == 0) {
+            int v1 = findKth(nums1, 0, m-1, nums2, 0, n-1, k-1);
+            int v2 = findKth(nums1, 0, m-1, nums2, 0, n-1, k);
+            return double(v1+v2)/2;
+        } else {
+            return findKth(nums1, 0, m-1, nums2, 0, n-1, k);
+        }
+    }
+    int findKth(vector<int>& nums1, int lo1, int hi1, vector<int>& nums2, int lo2, int hi2, int k) {
+        if(lo1 > hi1) return nums2[lo2+k];
+        if(lo2 > hi2) return nums1[lo1+k];
+        int mid1 = lo1 + (hi1-lo1)/2;
+        int mid2 = lo2 + (hi2-lo2)/2;
+        int hlen1 = mid1 - lo1, hlen2 = mid2 - lo2; //number of elements before mid
+        if(nums1[mid1] <= nums2[mid2]) {
+            if(k <= hlen1 + hlen2) return findKth(nums1, lo1, hi1, nums2, lo2, mid2-1, k); //drop elements after mid2 (inclusive)
+            else return findKth(nums1, mid1+1, hi1, nums2, lo2, hi2, k-hlen1-1); //drop elements before mid1 (inclusive)
+        } else {
+            if(k <= hlen1 + hlen2) return findKth(nums1, lo1, mid1-1, nums2, lo2, hi2, k); //drop elements after mid1 (inclusive)
+            else return findKth(nums1, lo1, hi1, nums2, mid2+1, hi2, k-hlen2-1); //drop elements before mid2 (inclusive)
+        }
+    }
 };
+
+// The same idea, iterative solutionclass Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        int k = (m+n)/2;
+        if((m + n) % 2 == 0) {
+            int v1 = findKth(nums1, 0, m-1, nums2, 0, n-1, k-1);
+            int v2 = findKth(nums1, 0, m-1, nums2, 0, n-1, k);
+            return double(v1+v2)/2;
+        } else {
+            return findKth(nums1, 0, m-1, nums2, 0, n-1, k);
+        }
+    }
+    int findKth(vector<int>& nums1, int lo1, int hi1, vector<int>& nums2, int lo2, int hi2, int k) {
+        while(lo1 <= hi1 && lo2 <= hi2) {
+            int mid1 = lo1 + (hi1-lo1)/2;
+            int mid2 = lo2 + (hi2-lo2)/2;
+            int hlen1 = mid1 - lo1, hlen2 = mid2 - lo2; //number of elements before mid
+            if(nums1[mid1] <= nums2[mid2]) {
+                if(k <= hlen1 + hlen2) hi2 = mid2-1; //drop elements after mid2 (inclusive)
+                else { lo1 = mid1+1; k -= hlen1+1; } //drop elements before mid1 (inclusive)
+            } else {
+                if(k <= hlen1 + hlen2) hi1 = mid1-1;  //drop elements after mid1 (inclusive)
+                else { lo2 = mid2+1; k -= hlen2+1; }  //drop elements before mid2 (inclusive)
+            }
+        }
+        if(lo1 > hi1) return nums2[lo2+k];
+        else return nums1[lo1+k];
+    }
+};
+
--- Algorithms/004_median_of_two_sorted_arrays_2.cc
+++ Algorithms/004_median_of_two_sorted_arrays_2.cc
@@ -34,3 +34,49 @@ public:
 		if((q == 0 || x >= B[q-1]) && (q == n || x <= B[q])) return m;
 	}
 };
+class Solution {
+public:
+	double findMedianSortedArrays(int A[], int m, int B[], int n) {
+		int k = (m+n)/2; 
+		if((m+n)%2 == 0) { //even
+			int a = Select(A, m, B, n, k);
+			int b = Select(A, m, B, n, k+1);
+			return double(a+b)/2.0;
+		} else return Select(A, m, B, n, k+1); //odd
+	}
+
+	//Binary search algorithm to selelect k-th order statistics from two SORTED arrays. Average O(log(m+n)) time.
+	int Select(int A[], int m, int B[], int n, int k) {
+		int idx = SelectUtil(A, B, n, 0, m-1, k); //search in A
+		if(m == 0) return B[k-1];
+		else if(n == 0) return A[k-1];
+		
+		if(idx != -1) return A[idx];
+		else {
+			idx = SelectUtil(B, A, m, 0, n-1, k); //search in B
+			return B[idx];
+		}
+	}
+    
+ 	//Utility function to find k-th order static in A+B in A[l .. r], B's size is n
+	int SelectUtil(int A[], int B[], int n, int l, int r, int k) {
+		if(r < l) return -1;
+		int m = (l+r)/2;
+		int x = A[m];
+		//x is <= m+1 elements in A, check if x is <= k-(m+1) elements in B
+		int q = k-m-1;
+		if(q < 0) return SelectUtil(A, B, n, l, m-1, k); //search left (A[l .. m-1])
+		else if(q > n) return SelectUtil(A, B, n, m+1, r, k); //search right (A[m+1 .. r])
+		else if(q == 0) {
+			if(x <= B[q]) return m;
+			else return SelectUtil(A, B, n, l, m-1, k);
+		} else if (q == n) {
+			if( x >= B[q-1]) return m;
+			else return SelectUtil(A, B, n, m+1, r, k);
+		} else {
+			if(x >= B[q-1] && x <= B[q]) return m;
+			else if (x > B[q]) return SelectUtil(A, B, n, l, m-1, k);
+			else return SelectUtil(A, B, n, m+1, r, k);
+		}
+	}
+};
