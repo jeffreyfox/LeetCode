06b8a94d4fcfae86256c41d421354f63e0f4c4ca
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-22 21:26:38 -0800

    003_longest_substr

--- Algorithms/003_longest_substr_wo_repeating_char.cc
+++ Algorithms/003_longest_substr_wo_repeating_char.cc
@@ -21,3 +21,23 @@ public:
         return max_len;
     }
 };
+
+// Another solution by counting the occurrences of each character, similar to problem 159.
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+         int n = s.size(); //number of records
+         int istart = 0, max_len = 0;
+         int count = 0;
+         vector<int> dict(256);
+         for(int i = 0; i < n; ++i) {
+            char c = s[i];
+            ++dict[c];
+            while(dict[c] > 1) {
+               --dict[s[istart++]];
+            }
+            max_len = max(max_len, i-istart+1);
+         }
+         return max_len;
+    }
+};
