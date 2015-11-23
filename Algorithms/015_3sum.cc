c744b1b016f0540b0fd94cd9da77f359edab8f0c
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-22 21:25:40 -0800

    015_3sum.cc

--- Algorithms/015_3sum.cc
+++ Algorithms/015_3sum.cc
@@ -50,3 +50,42 @@ public:
     }
 };
 
+/// O(n2 lgn) solution. Iterate first 2 brute-force, and use binary search to find 3rd element.
+/// Need to avoid duplicated entries. (lines 14 and 16).
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int> > ret;
+        
+        int n = nums.size();
+        if(n <= 2) return ret;
+        vector<int> sol(3, 0);
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n; ++i) { //whether nums[i] is one of the triplet?
+            if(i > 0 && nums[i] == nums[i-1]) continue; //ignore duplicates
+            for(int j = i+1; j < n; ++j) {
+                if(j > i+1 && nums[j] == nums[j-1]) continue; //ignore duplicates
+                int target = -(nums[i] + nums[j]);
+                int r = find(nums, j+1, target);
+                if(r != -1) {
+                    sol[0] = nums[i], sol[1] = nums[j], sol[2] = nums[r];
+                    ret.push_back(sol);
+                }
+            }
+        }
+        return ret;
+    }
+    
+    //search target in sorted array nums[k..end)
+    int find(vector<int>& nums, int k, int target) {
+        int lo = k, hi = nums.size()-1;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(target < nums[mid]) hi = mid-1;
+            else if(target > nums[mid]) lo = mid+1;
+            else return mid;
+        }
+        return -1;
+    }
+};
