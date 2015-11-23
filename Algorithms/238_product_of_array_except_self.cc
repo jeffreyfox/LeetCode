4a096ddcd7b16dde9f0f5d589092966675e4bf65
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-22 19:04:48 -0800

    238_product_of_array_except-self

--- Algorithms/238_product_of_array_except_self.cc
+++ Algorithms/238_product_of_array_except_self.cc
@@ -1,3 +1,14 @@
+/*
+Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+*/
+
 /// O(n) time, O(1) space solution.
 /// Two passes, one from left and one from right
 /// Product without self = product of all elements to the left, times product of all elements to the right
