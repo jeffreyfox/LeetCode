2a88c3a72736c7891823b035d4fddf333adf15c1
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-24 08:46:58 -0800

    121_best_time_stock

--- Algorithms/121_best_time_to_buy_and_sell_stock.cc
+++ Algorithms/121_best_time_to_buy_and_sell_stock.cc
@@ -1,14 +1,23 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+*/
+
 /// Greedy one pass O(n) solution:
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int max_so_far = 0;
-        int max_all = 0;
-        for (int i = 1; i < prices.size(); ++i) {
-            max_so_far += (prices[i]-prices[i-1]);
-            if (max_so_far < 0) max_so_far = 0;
-            else max_all = max(max_all, max_so_far);
+        int n = prices.size();
+        if(n <= 1) return 0;
+        int max_so_far = 0, max_all = 0;
+        for(int i = 1; i < n; ++i) {
+            int change = prices[i] - prices[i-1];
+            if(max_so_far <= 0) max_so_far = change;
+            else max_so_far += change;
+            max_so_far = max(0, max_so_far);
+            max_all = max(max_so_far, max_all);
         }
         return max_all;
     }
