4ecf4a505ae7770ee988998beade071a984963af
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-25 19:00:49 -0800

    parentheses

--- Algorithms/032_longest_valid_parentheses.cc
+++ Algorithms/032_longest_valid_parentheses.cc
@@ -6,28 +6,27 @@ For "(()", the longest valid parentheses substring is "()", which has length = 2
 Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4. 
 */
 
-/// Dynamic programming, use a table to store the length of longest valid parentheses of s's substring of length i (i = 0 .. n)
-/// Use a stack to track the position of '('. When seeing a '(', insert to stack; when seeing a ')', pop stack and find the index of last matching '('.
-// Example: say at i we find that the matching bracket is at j. Then len[i+1] = len[j] + i-j+1
-//    (  ...  )  ( ... ) 
-//           j-1 j     i
-//     len[j]     i-j+1
+/*
+ Dynamic programming, use a table to store the length of longest valid parentheses of s's substring of length i (i = 0 .. n)
+ We scan all characters, for i-th character, when we see a ')' we start checking the substring s[0..i-1] of length i. We already know the maximum length of s[0..i), so we check the character before that, if it is '(', then we have found a matching bracket, we further add the part before that '(' is there is any. Be attention of case of possible array out of bound.
+
+  s: 0    ..       j   ...    i-1  i
+         [ ... ]  '('   [ ... ]   ')'
+*/ 
 
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        if(s.empty()) return 0;
-        int max_len = 0;
         int n = s.size();
-        deque<int> pos; //stack storing position of '('
-        vector<int> len(n+1, 0); //len[i]: max length for the LVP of s[0..i-1] (length of i)
+        if(n <= 1) return 0;
+        vector<int> mp(n+1, 0); //mp[n]: the length of lvp substring of s[0..n-1], length n
+        int max_len = 0;
         for(int i = 0; i < n; ++i) {
-            if(s[i] == '(') pos.push_back(i);
-            else if(!pos.empty()) { //find matching '(' for ')'
-                int j = pos.back();
-                len[i+1] = len[j] + i-j+1;
-                pos.pop_back();
-                max_len = max(max_len, len[i+1]);
+            if(s[i] == ')') {
+                int len = mp[i]; //check previous substring
+                if(mp[len] >= i || s[i-len-1] != '(') mp[i+1] = 0; //no valid substring ending at s[i]
+                else mp[i+1] = len + mp[i-len-1] + 2;
+                max_len = max(max_len, mp[i+1]);
             }
         }
         return max_len;
--- Algorithms/241_different_ways_to_add_parentheses.cc
+++ Algorithms/241_different_ways_to_add_parentheses.cc
@@ -23,7 +23,64 @@ Input: "2*3-4*5"
 Output: [-34, -14, -10, -10, 10]
 */
 
-// Divide and conquer. Final vector is the cartesian product of two sub-vectors
+// Dynamic programming solution.
+// First parse string and save numbers and operators separately.
+// Then start from bottom up, for large string break into small pieces and check all options.
+
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string input) {
+        if(input.empty()) return vector<int>();
+        vector<int> nums; 
+        vector<char> ops;
+        parse(input, nums, ops);
+        int n = nums.size(); //number of numbers
+        //mp[i][j]: outcomes from i (i = [0..n]) numbers starting at j (j = [0 .. n-1]). 
+        vector<vector<vector<int> > > mp(n+1, vector<vector<int> >(n, vector<int>()));
+        //len = 1 case
+        for(int i = 0; i < n; ++i) {
+           mp[1][i].push_back(nums[i]);
+        }
+        for(int len = 2; len <= n; ++len) {
+            for (int i = 0; i + len <= n; ++i) {
+                int j = i + len - 1; //i start, j end, inclusive
+                //try to break it from i+1 .. to j-1
+                for(int k = i; k < j; ++k) { // break like this: num[k] | op[k] | num[k+1]
+                    for(auto it1 : mp[k-i+1][i]) {
+                        for(auto it2 : mp[j-k][k+1]) {
+                            mp[len][i].push_back(eval(it1, it2, ops[k]));
+                        }
+                    }
+                }
+            }
+        }
+        return mp[n][0];
+    }
+    
+    int eval(int v1, int v2, char op) {
+       if(op == '+') return v1+v2;
+       if(op == '-') return v1-v2;
+       if(op == '*') return v1*v2;
+       return 0;
+    }
+
+    void parse(string& input, vector<int>& nums, vector<char>& ops) {
+        int val = 0;
+        for(int i = 0; i < input.size(); ++i) {
+            char c = input[i];
+            if(isdigit(c)) {
+               val = 10*val + (c - '0');
+            } else {
+               ops.push_back(c);
+               nums.push_back(val);
+               val = 0;
+            }
+        }
+        nums.push_back(val); //don't forget last one!
+    }
+};
+
+// Recursive solution using divide and conquer. Final vector is the cartesian product of two sub-vectors
 
 class Solution {
 public:
