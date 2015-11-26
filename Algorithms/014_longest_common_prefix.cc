8be083c6fefe94fe4cafe4c5f48c2c2f53c2574b
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-25 19:16:03 -0800

    014_longest_common_prefix

--- Algorithms/014_longest_common_prefix.cc
+++ Algorithms/014_longest_common_prefix.cc
@@ -1,5 +1,29 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings. 
+*/
+
+// Compare all strings against first string. Two nested loops. When found a mismatch, simply return the maximum length found so far.
+// If no mismatch found, return full first string
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+        int n = strs.size();
+        if(n == 1) return strs[0];
+        int ns = strs[0].size();
+        for(int j = 0; j < ns; ++j) { //j loops over each string
+            for(int i = 1; i < n; ++i) { //i loops over vector of strings
+                if(j == strs[i].size() || strs[i][j] != strs[0][j])  //found a mismatch!
+                    return strs[0].substr(0, j); //return s[0, j)
+            }
+        }
+        return strs[0];
+    }
+};
+
+// Another solution, need to break out two nested loops.
 // Compare all strings against first string
-// Need two nested loops, when break, break two loops
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
