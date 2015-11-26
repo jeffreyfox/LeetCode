cab029331d103cb8c116410e47811b6c15621cc5
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-25 18:23:10 -0800

    022_generate_parenthesis

--- Algorithms/022_generate_parenthesis.cc
+++ Algorithms/022_generate_parenthesis.cc
@@ -1,3 +1,40 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+"((()))", "(()())", "(())()", "()(())", "()()()" 
+*/
+
+// Recursive solution. backtracking, DFS
+// pre-allocate string, pass reference and string index to avoid string copying. Keep track of number of open and close brackets left.
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        if(n < 1) return result;
+        string tmp(2*n, '0');
+        dfs(n, n, 0, tmp, result);
+        return result;
+    }
+    void dfs(int nOpen, int nClose, int i, string& tmp, vector<string>& result) {
+        if(nOpen < 0 || nClose < 0 || nOpen > nClose) return;
+        
+        if(i == tmp.size()) {
+            result.push_back(tmp);
+            return;
+        }
+        
+        tmp[i] = '(';
+        dfs(nOpen-1, nClose, i+1, tmp, result);
+        
+        tmp[i] = ')';
+        dfs(nOpen, nClose-1, i+1, tmp, result);
+    }
+};
+
+// Another iterative solution
 class Solution {
 public:
 	vector<string> generateParenthesis(int n) {
