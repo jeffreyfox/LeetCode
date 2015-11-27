b4f468765679dd01ffbd781acb96cf86910957d1
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-26 21:33:13 -0800

    066_plus_one

--- Algorithms/066_plus_one.cc
+++ Algorithms/066_plus_one.cc
@@ -1,15 +1,26 @@
+/*
+Given a non-negative number represented as an array of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
+*/
+
+// Optimization: copy digits to result, and then stop when carry == 0
+
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        vector<int> result;
+        if(digits.empty()) return result;
         int n = digits.size();
-        vector<int> ret(n, 0);
-        int carry = 1;
-        for (int i = n-1; i >=0; --i) {
-            int sum = digits[i] + carry;
-            ret[i] = sum % 10;
+        result = digits;
+        int carry = 1, sum = 0;
+        for(int i = n-1; i >= 0 && carry > 0; i--) {
+            sum = digits[i] + carry;
             carry = sum / 10;
+            sum %= 10;
+            result[i] = sum; //update result
         }
-        if (carry == 1) ret.insert(ret.begin(), 1);
-        return ret;
+        if(carry > 0) result.insert(result.begin(), 1); //add a leading 1
+        return result;
     }
 };
