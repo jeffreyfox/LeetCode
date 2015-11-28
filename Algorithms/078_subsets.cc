a116c22259f19bbf8b44476d11cc20e48605c3a8
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-28 14:41:58 -0800

    subsets

--- Algorithms/078_subsets.cc
+++ Algorithms/078_subsets.cc
@@ -1,23 +1,110 @@
+/*
+Given a set of distinct integers, nums, return all possible subsets.
+
+Note:
+
+    Elements in a subset must be in non-descending order.
+    The solution set must not contain duplicate subsets.
+
+For example,
+If nums = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+
+// Solution 1. Recursive backtracking using DFS. Fan-out is 2 (for each number, add or not add).
+
 class Solution {
 public:
-	vector<vector<int> > subsets(vector<int> &S) {
-		vector<vector<int> > ret;
-		vector<int> path;
-		sort(S.begin(), S.end());
-		iterative(S, 0, path, ret);
-		return ret;
-	}
-	//get letter for digit[i]
-	void iterative(const vector<int>& S, int beg, 
-	               const vector<int>& path, vector<vector<int> >& ret) {
-		if(beg == S.size())	ret.push_back(path);		   
-		else {
-			ret.push_back(path);
-			for(int k = beg; k < S.size(); ++k) { //choose among possible candidates
-				vector<int> newpath(path);
-				newpath.push_back(S[k]);
-				iterative(S, k+1, newpath, ret);
-			}
-		}
-	}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int> > result;
+        sort(nums.begin(), nums.end());
+        vector<int> tmp;
+        dfs(nums, 0, tmp, result);
+        return result;
+    }
+    void dfs(vector<int>& nums, int i, vector<int>& tmp, vector<vector<int> >& result) {
+        if(i == nums.size()) {
+            result.push_back(tmp);
+            return;
+        }
+        // not taking nums[i]
+        dfs(nums, i+1, tmp, result);
+        // taking nums[i]
+        tmp.push_back(nums[i]);
+        dfs(nums, i+1, tmp, result);
+        tmp.pop_back(); //revert
+    }
 };
+
+// Solution 2. Recursive backtracking using DFS. Fan-out is number of unused elements, less tree depth and thus better efficiency than 1.
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int> > result;
+        vector<int> tmp;
+        sort(nums.begin(), nums.end());
+        dfs(nums, 0, tmp, result);
+        return result;
+    }
+    void dfs(vector<int>& nums, int i, vector<int>& tmp, vector<vector<int> >& result) {
+        result.push_back(tmp);
+        for(int j = i; j < nums.size(); ++j) { //choose one from unused numbers
+            tmp.push_back(nums[j]);
+            dfs(nums, j+1, tmp, result);
+            tmp.pop_back();
+        }
+    }
+};
+
+// Solution 3. Dynamic programming. For each number, add to previous solution.
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int> > result;
+        sort(nums.begin(), nums.end());
+        result.push_back(vector<int>()); //empty set
+        for(int i = 0; i < nums.size(); ++i) {
+            //add nums[i] to all previous solutions
+            int size = result.size();
+            for(int j = 0; j < size; ++j) {
+                result.push_back(result[j]);
+                result.back().push_back(nums[i]);
+            }
+        }
+        return result;
+    }
+};
+
+// Solution 4. Use bit operation. There are pow(2, n) results, we can label them in a consistent order similar to Solution 3.
+// for example [1,2,3]:
+// subset:       [], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]
+// id(base 10):  0    1    2      3     4     5       6         7
+// id(base 2):  000  001  010    011   100   101     110       111
+// As we can see, number 1 (the 0-th number) only appears when last digit is set. number 2 (the 1st number) only appears when second from last digit is set, and so on. Thus, for each id, we can check which digit are set, and add the corresponding number to the subset.
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        int size = 1 << n;
+        sort(nums.begin(), nums.end());
+        vector<vector<int> > result(size, vector<int>());
+        for(int i = 0; i < result.size(); ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(i & (1 << j)) result[i].push_back(nums[j]);
+            }
+        }
+        return result;
+    }
+};
+
--- Algorithms/090_subsets_ii.cc
+++ Algorithms/090_subsets_ii.cc
@@ -1,26 +1,102 @@
+/*
+Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+
+Note:
+
+    Elements in a subset must be in non-descending order.
+    The solution set must not contain duplicate subsets.
+
+For example,
+If nums = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+*/
+
+// Solution 1. Similar to #78, DFS backtracking with fan-out = 2.
 class Solution {
 public:
-    vector<vector<int> > subsetsWithDup(vector<int> &S) {
-		vector<vector<int> > ret;
-		vector<int> path;
-		sort(S.begin(), S.end());
-		iterative(S, 0, path, ret);
-		return ret;
-	}
-	//get letter for digit[i]
-	void iterative(const vector<int>& S, int beg, 
-	               const vector<int>& path, vector<vector<int> >& ret) {
-		if(beg == S.size())	ret.push_back(path);		   
-		else {
-			ret.push_back(path);
-			int prev = INT_MIN;
-			for(int k = beg; k < S.size(); ++k) { //choose among possible candidates
-				if(S[k] == prev) continue;
-				vector<int> newpath(path);
-				newpath.push_back(S[k]);
-				iterative(S, k+1, newpath, ret);
-				prev = S[k];
-			}
-		}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int> > result;
+        sort(nums.begin(), nums.end());
+        vector<int> tmp;
+        dfs(nums, 0, tmp, result);
+        return result;
+    }
+    void dfs(vector<int>& nums, int i, vector<int>& tmp, vector<vector<int> >& result) {
+        if(i == nums.size()) {
+            result.push_back(tmp);
+            return;
+        }
+        int j = i;
+        while(j < nums.size() && nums[j] == nums[i]) j++;
+        //j is index of first element != nums[i], or end
+        //not adding nums[i] to set
+        dfs(nums, j, tmp, result);
+        //adding 1 to j-i nums[i] to set
+        for(int k = 0; k < j-i; ++k) {
+            tmp.push_back(nums[i]);
+            dfs(nums, j, tmp, result);
+        }
+        //revert back
+        for(int k = 0; k < j-i; ++k) {
+            tmp.pop_back();
+        }
     }
 };
+
+// Solution 2. Similar to #78, DFS backtracking with fan-out = number of unused elements
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int> > result;
+        vector<int> tmp;
+        sort(nums.begin(), nums.end());
+        dfs(nums, 0, tmp, result);
+        return result;
+    }
+    void dfs(vector<int>& nums, int i, vector<int>& tmp, vector<vector<int> >& result) {
+        result.push_back(tmp);
+        for(int j = i; j < nums.size(); ++j) {
+            if(j == i || nums[j] != nums[j-1]) { //ignore duplicates
+                tmp.push_back(nums[j]);
+                dfs(nums, j+1, tmp, result);
+                tmp.pop_back();
+            }
+        }
+    }
+};
+
+// Solution 3. Similar to #78, dynamic programming.
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int> > result;
+        sort(nums.begin(), nums.end());
+        result.push_back(vector<int>()); //empty set
+        int i = 0;
+        int n = nums.size();
+        while(i < n) {
+            int j = i;
+            while(j < n && nums[j] == nums[i]) j++;
+            int ndup = j-i; //number of duplicates
+            //add 1 - ndup nums[i] to existing solution
+            int size = result.size();
+            for(int k = 0; k < ndup; ++k) {
+                for(int l = k*size; l < (k+1)*size; ++l) {
+                    result.push_back(result[l]);
+                    result.back().push_back(nums[i]);
+                }
+            }
+            i = j;
+        }
+        return result;
+    }
+};
+
