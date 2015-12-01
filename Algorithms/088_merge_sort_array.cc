021de3c6b407240f5bfa8d52ba30bf2282b33eae
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-30 22:14:28 -0800

    088_merge_sort_array.cc

--- Algorithms/088_merge_sort_array.cc
+++ Algorithms/088_merge_sort_array.cc
@@ -1,4 +1,35 @@
-// Merge procedure in merge sort. No sentinel. Need extra array of length O(m+n).
+/*
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+*/
+
+// Best solution: start backwards, utilize the extra space after nums1, only O(1) extra space needed.
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1, j = n-1, k = m+n-1;
+        while(i >= 0 || j >= 0) {
+            if(j < 0) break;
+            else if(i < 0) nums1[k--] = nums2[j--];
+            else nums1[k--] = nums1[i] <= nums2[j] ? nums2[j--] : nums1[i--];
+       }
+    }
+};
+
+// A slightly more concise solution
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1, j = n-1, k = m+n-1;
+        while(j >= 0) {
+            nums1[k--] = (i < 0 || nums1[i] <= nums2[j]) ? nums2[j--] : nums1[i--];
+       }
+    }
+};
+
+// An old solution: merge procedure in merge sort. No sentinel. Need extra array of length O(m+n).
 
 class Solution {
 public:
