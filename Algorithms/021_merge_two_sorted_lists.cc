2b7d093ec0f73f35cf82adcd96da5dc10f618876
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-11-30 22:03:24 -0800

    merge

--- Algorithms/021_merge_two_sorted_lists.cc
+++ Algorithms/021_merge_two_sorted_lists.cc
@@ -1,3 +1,7 @@
+/*
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -7,20 +11,24 @@
  * };
  */
 
-// Use dummy head
+// Use dummy head, and a tail pointer to track end of sorted list.
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
-        ListNode dum(0), *p(&dum), *q(p);
+        ListNode dummy(0), *p(&dummy);
+        ListNode *tail = p;
         while(l1 && l2) {
-            if (l1->val < l2->val) { q->next = l1; l1 = l1->next; }
-            else { q->next = l2; l2 = l2->next; }
-            q = q->next;
+            if(l1->val <= l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
         }
-        if(l1 != NULL) q->next = l1;
-        else q->next = l2;
+        if(l1) tail->next = l1;
+        else tail->next = l2;
         return p->next;
     }
 };
--- Algorithms/023_merge_k_sorted_lists.cc
+++ Algorithms/023_merge_k_sorted_lists.cc
@@ -1,3 +1,7 @@
+/*
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity. 
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -7,41 +11,159 @@
  * };
  */
  
-// Divide and conquer solution
+// Divide and conquer solution. Use the merge-two-list function.
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int size = lists.size();
+        if(size == 0) return NULL;
+        int i = 0;
+        while(size > 1) {
+            for(i = 0; 2*i+1 < size; i++) {
+                lists[i] = mergeTwoLists(lists[2*i], lists[2*i+1]);
+            }
+            if(size % 2 == 1) lists[i] = lists[size-1];
+            size = (size+1)/2;
+        }
+        return lists[0];
+    }
+
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0), *p(&dummy);
+        ListNode *tail = p;
+        while(l1 && l2) {
+            if(l1->val <= l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        if(l1) tail->next = l1;
+        else tail->next = l2;
+        return p->next;
+    }
+};
+
+// Priority queue solution O(nlgk) using STL priority queue, and comparator class
+class isGreater {
+public:
+    bool operator() (ListNode *a, ListNode *b) {
+        return a->val > b->val;
+    }
+};
+
+class Solution {
+
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, isGreater> q;
+        for(int i = 0; i < lists.size(); ++i) {
+            if(lists[i]) q.push(lists[i]);
+        }
+        if(q.empty()) return NULL;
+        ListNode dummy(0), *p(&dummy);
+        ListNode *tail = p;
+        while(q.size() > 1) {
+            ListNode *node = q.top(); q.pop();
+            tail->next = node;           
+            tail = tail->next;
+            if(node->next) q.push(node->next);
+        }
+        // only one element left
+        tail->next = q.top();
+        return p->next;
+    }
+};
+
+// Priority queue solution O(nlgk) without using STL priority queue
+class MinHeap {
+public:
+	MinHeap(int max_l) : maxL(max_l), size(0) {
+		head = new ListNode* [maxL];
+		arr = head-1; //pointer such that first element is arr[1]
+	}
+	~MinHeap() { delete [] head;}
+	
+    //push back without FixUp
+	void PushBack(ListNode* node) {
+		head[size++] = node;
+	}
+    //insert and run FixUp 
+    void Insert(ListNode* node) {
+        head[size++] = node;
+        FixUp(size);
+    }
+	//build heap
+	void BuildHeap() {
+		for(int i = size/2; i >= 1; --i) FixDown(i);
+	}
+
+	bool Empty() { return size == 0; }
+
+	// Extract minimum element
+	ListNode* ExtractMin() {
+		Swap(arr[size--], arr[1]);
+		FixDown(1);
+		return arr[size+1];
+	}
+
+	//fix down (check if arr[i] needs to be swapped with children)
+	void FixDown(int i) {
+		while(i > 0) {
+			int smallest = i;
+			int l = Left(i), r = Right(i);
+			if( l <= size && arr[l]->val < arr[smallest]->val) smallest = l;
+			if( r <= size && arr[r]->val < arr[smallest]->val) smallest = r;
+			if(smallest != i) {
+				Swap(arr[smallest], arr[i]);
+				i = smallest;
+			} else break;
+		}
+	}
+    //fix up (check if arr[i] needs to be swapped with parent)
+	void FixUp(int i) {
+		while(i > 1) {
+			int p = Parent(i);
+			if(arr[p]->val > arr[i]->val) {
+				Swap(arr[p], arr[i]);
+				i = p;
+			} else break;
+		}
+	}
+
+	int Left(int i) {return 2*i;}
+	int Right(int i) {return 2*i+1;}
+	int Parent(int i) {return i/2;}
+	void Swap(ListNode* &a, ListNode* &b) { ListNode* t = a; a = b; b = t;}
+
+private:
+	ListNode** head; //point to the head of array
+	ListNode** arr; //one less the beginning of array
+	int maxL; //max length
+	int size; //size
+};
+
 class Solution {
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists) {
         int size = lists.size();
-		if(size == 0) return NULL;
-		while(size > 1) {
-			for(int k = 0; k < size; k += 2) {
-				if(k+1 < size) lists[k/2] = mergeTwoLists(lists[k], lists[k+1]);
-				else lists[k/2] = lists[k];
-			}
-			size = (size+1)/2;
-		}
-		return lists[0];
+        if(size == 0) return NULL;
+        ListNode dum(0), *dummy(&dum), *p(dummy);
+        MinHeap hp(size);
+        for(int i = 0; i < size; ++i) {
+            if(lists[i]) hp.PushBack(lists[i]);
+        }
+        hp.BuildHeap();
+        while(!hp.Empty()) {
+            ListNode *min_node = hp.ExtractMin();
+            p->next = min_node;
+            if(min_node->next) hp.Insert(min_node->next);
+            p = p->next;
+        }
+        p->next = NULL;
+        return dummy->next;
     }
-    
-    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)  {
-		ListNode dum(0);
-		ListNode *dummy(&dum);
-		ListNode *tail = dummy;
-		while(l1 && l2) {
-			if(l1->val <= l2->val) { //append l1 to tail
-				tail->next = l1;
-				tail = l1;
-				l1 = l1->next;
-			} else { //append l2 to tail
-				tail->next = l2;
-				tail = l2;
-				l2 = l2->next;
-			}
-		}
-		//at this point either l1 or l2 is null
-		if(l1) tail->next = l1;
-		else if(l2) tail->next = l2;
-		else tail->next = NULL;
-		return dummy->next;
-	}
 };
