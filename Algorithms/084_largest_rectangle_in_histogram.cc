b6b5d31c833cc396359f5c032f29ed1438f50d47
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-12-05 09:42:17 -0800

    084_largest_rectangle_in_histogram

--- Algorithms/084_largest_rectangle_in_histogram.cc
+++ Algorithms/084_largest_rectangle_in_histogram.cc
@@ -10,28 +10,87 @@ Given height = [2,1,5,6,2,3],
 return 10. 
 */
 
-/// using a stack to store positions. Compare the newly encountered height with that in the stack. 
-/// If higher, push into stack. If lower, then we know that the previous height can only reach left of here.
-/// Initially push a sentinel element to back of height array
+/*
+The rectangle is limited by the histrogram bar of the lowest height.
+Scan the histogram one-by-one. Say we already scanned i bars, and the next is i+1. There are three cases:
+1. i+1 is higher than i. In this case, we can't say anything about the possible rectangles formed by i, so we just push i+1 to stack.
+
+     ++++
+     +  +
+  ++++  +
+  +  +  +
+  +  +  +
++++++++++++++++++++++++++++++++++++++++
+   i  i+1
+
+2. i+1 is lower than i. In this case, we can say for sure that the largest rectangle that bar i can form will terminate at i+1. So we calculate that area, and pop-out i (since we don't need i anymore)
+
+  ++++
+  +  +
+  +  ++++
+  +  +  +
+  +  +  +
++++++++++++++++++++++++++++++++++++++++
+   i  i+1
+
+The tricky part is, when we pop-out i, i+1 can be extended to the left, since it is lower, and the largest rectangle formed by i+1 can extend to the left, so we have to store not only the height but also the current left boundary position of the rectangle formed by i. After removing i effectively we will have, which waits to be checked in the next step.
+
+
+  +++++++
+  +     +
+  +     +
++++++++++++++++++++++++++++++++++++++++
+   i  i+1
+
+3. i+1 is same height as i. We can still use 2, because i+1 will inherit the position of i.
+
+*/
+
+/// Solution 1 using a stack of both position and height. No need to handle degenerate cases separately.
+/// We use a variable j to store the position of recently popped bars, and make the new bar inherit this position when inserted to stack.
+/// Another trick is to append a sentinel of height 0 in the end of height vector, so that all the bars in the stack will eventually be popped out to be examined.
 
 class Solution {
 public:
     int largestRectangleArea(vector<int>& height) {
         if(height.empty()) return 0;
-        height.push_back(0);
-        deque<int> s; //stack for positions
-        int n = height.size();
         int max_area = 0;
+        stack<pair<int, int> > st; //store position and height 
+        height.push_back(0); //sentinel at the end
+        int n = height.size();
         for(int i = 0; i < n; ++i) {
-             while(!s.empty() && height[s.back()] > height[i]) {
-                int h = height[s.back()];
-                s.pop_back();
-                int j = s.empty() ? 0 : s.back()+1;
-                max_area = max(max_area, h*(i-j));
+            int j = i, h = 0;
+            while(!st.empty() && st.top().second > height[i]) {
+                j = st.top().first;
+                h = st.top().second;
+                max_area = max(max_area, (i-j)*h);
+                st.pop();
             }
-            s.push_back(i);
+            st.push(make_pair(j, height[i])); //inherits position j from popped elements
         }
         return max_area;
     }
+};
 
+/// Solution 2 using a stack only tracking the index of bar in the height. We don't need to store the inherited position, because we observe that the inherited position is always one plus the position of the bar at top of stack after the bar currently under examination is popped out.
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& height) {
+        if(height.empty()) return 0;
+        int max_area = 0;
+        stack<int> st; //store position in height
+        height.push_back(0); //sentinel at the end
+        int n = height.size();
+        for(int i = 0; i < n; ++i) {
+            while(!st.empty() && height[st.top()] > height[i]) {
+                int h = height[st.top()];
+                st.pop();
+                int j = st.empty() ? 0 : st.top() + 1;
+                max_area = max(max_area, (i-j)*h);
+            }
+            st.push(i);
+        }
+        return max_area;
+    }
 };
