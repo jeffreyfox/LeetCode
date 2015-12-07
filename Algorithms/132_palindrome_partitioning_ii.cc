9b40201c69bee098b2803a5fcb02e24f858b548e
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-12-06 16:50:16 -0800

    132_palindrome_parittioning

--- Algorithms/132_palindrome_partitioning_ii.cc
+++ Algorithms/132_palindrome_partitioning_ii.cc
@@ -7,16 +7,18 @@ For example, given s = "aab",
 Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut. 
 */
 
-// Dynamic programming.
+// Dynamic programming. Store 1-d array indicating the min-cut for s[0, i) prefix. 
+// Update the minCuts for substrings as we check the strings.
+// j = 0 is needed in this case.
 
 class Solution {
 public:
     int minCut(string s) {
         if(s.empty()) return 0;
         int n = s.size();
-        vector<int> minCuts(n+1, INT_MAX); //min-cuts for s's length-i prefix (i = 0 .. n)
-        minCuts[0] = -1; minCuts[1] = 0; // define minCut for empty string as -1 for later convenience
-        for (int i = 0; i < n; ++i) { //check s[i]
+        vector<int> minCuts(n+1, INT_MAX); //min-cuts for s[0, i)
+        minCuts[0] = -1; // define minCut for empty string as -1 for later convenience
+        for (int i = 0; i < n; ++i) { //check if s[i] is part of a palindrome substring
             //could s[i] be in the middle of odd-length palindrome?
             for (int j = 0; i >= j && i+j < n && s[i-j] == s[i+j]; ++j) //j = 0 is needed to init minCuts[i+1]
                 minCuts[i+j+1] = min(minCuts[i+j+1], minCuts[i-j]+1);
@@ -27,3 +29,4 @@ public:
         return minCuts[n];
     }
 };
+
