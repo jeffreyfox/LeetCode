230ea2b853e81d0196fafd2d4daf2d5701dc3475
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-12-08 09:22:33 -0800

    025_reverse_k_group

--- Algorithms/025_reverse_nodes_in_k_group.cc
+++ Algorithms/025_reverse_nodes_in_k_group.cc
@@ -1,3 +1,20 @@
+/*
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+You may not alter the values in the nodes, only nodes itself may be changed.
+
+Only constant memory is allowed.
+
+For example,
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5 
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -7,11 +24,54 @@
  * };
  */
 
-/// Determine the head and tail of each segment, and use a helper function to reverse it, and attach it to original list. The helper funtion
-/// uses recursive algorithm to reverse a segment
-/// while loop invariant:
-/// start:   last -> head -> ... -> ... -> tail -> next 
-/// end:     last -> tail -> ... -> ... -> head -> next 
+/*
+Solution 1. Iterative.
+Determine the head and tail of each segment, and use a helper function to reverse it, and attach it to original list. The helper funtion
+Uses a dummy head.
+while loop invariant:
+ start:   p1 -> [ p2 -> ... -> p3 ] -> p4 
+ end:     p1 -> [ p3 -> ... -> p2 ] -> p4
+*/
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(!head || k <= 1) return head;
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *p1 = dummy, *p2 = head;
+       // p1 -> [ p2 -> ... -> p3 ] -> p4 => p1 -> p3 -> ... -> p2 -> p4
+        while(p2) {
+            int count = k;
+            ListNode *p3 = p2, *p4 = NULL;
+            while(count > 1 && p3) {
+                count--;
+                p3 = p3->next;
+            }
+            if(!p3) break; //not enough elements to reverse
+            p4 = p3->next;
+            //now reverse p2 -> ... -> p3
+            ListNode *curr = p2, *next = curr->next, *nextnext = NULL;
+            while(curr != p3) {
+                nextnext = next->next; // save it to tmp
+                next->next = curr; //reverse 
+                curr = next; //move forward
+                next = nextnext; //move forward
+            }
+            //now connects p1 -> p3 and p2 -> p4
+            p1->next = p3; p2->next = p4;
+            //back to loop
+            p1 = p2; p2 = p4;
+        }
+        head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
+
+/* 
+Solution 2. Same as method 1, but use iterative helper function to reverse.
+*/
 
 class Solution {
 public:
@@ -35,11 +95,25 @@ public:
         }
         return p->next;
     }
-    // reverse a list segment (head -> ... -> tail), which becomes (tail -> ... -> head)
     void reverse(ListNode* head, ListNode* tail) {
-        if(head == tail) return;
-        ListNode *next = head->next;
-        reverse(next, tail);
-        next->next = head;
+        ListNode *next(head->next), *nextnext(NULL);
+        while (head != tail) {
+            nextnext = next->next;
+            next->next = head; //reverse
+            head = next;
+            next = nextnext;
+        }
     }
+/*  
+    //A slightly different reverse version
+    void reverse(ListNode* head, ListNode* tail) {
+        ListNode *next(head->next);
+        while (head != tail) {
+            ListNode* tmp = next->next; //tmp stores next->next 
+            next->next = head; //reverse
+            head = next;
+            next = tmp;
+        }
+    }
+*/
 };
