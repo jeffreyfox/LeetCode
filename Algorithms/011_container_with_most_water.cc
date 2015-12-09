b3e6c9f1d3ee3943fc30f008fb1e91012d6442fe
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-12-08 20:25:54 -0800

    water

--- Algorithms/011_container_with_most_water.cc
+++ Algorithms/011_container_with_most_water.cc
@@ -1,9 +1,19 @@
-/*Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+/*
+Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
 
 Note: You may not slant the container. 
 */
 
-//Similar idea to "trapping rain water". Maintain two pointers and find the minimum water level deterimed by the two pointers. Move from the side with lower height, stop when two pointers meet
+/*
+Similar idea to "trapping rain water". Maintain two pointers and find the minimum water level deterimed by the two pointers. Move from the side with lower height, stop when two pointers meet.
+The height of container is the smaller of the two ends. Proceed by moving the lower point inwards, because we can proove that the interval between lo and hi is the widest one possible for the container of height of height[lo].
+Proceeding from higher point is wrong! e.g.
+height:   3 9 1 1
+position: 0 1 2 3
+
+l = 0, h = 3 => area = 3
+if we move from higher point, we missed [0, 1] with area 6.
+*/
 
 class Solution {
 public:
--- Algorithms/042_trapping_rain_water.cc
+++ Algorithms/042_trapping_rain_water.cc
@@ -5,6 +5,45 @@ For example,
 Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6. 
 */
 
+/*
+We notice that the amount of water trapped at position[i] is bounded by the *lower* of the peak position at its left and peak position at its right. This has suggested a two-way search, instead of single-direction scan. We use two pointers, l and r, initially at the two boundaries of array. Then, the water level is limited by the lower of hl = height[l] and hr = height[r]. We move from the lower side towards the other side., here we assume it is the left side. We also keep track of the current water level at each position. Two cases:
+1) if we see a higher point than hl. this will raise the water level to the new height. No water can be trapped here.
+2) if we see a lower point than hl, then water will be trapped here and the amount is the difference between current water level and current height at this position.
+
+Every time we move one step, we compare height[l] and height[r], and we always move from the lower side until the two meets.
+
+Caveats:
+store height[l] and height[r] as temporary hl and hr, so that when l or r moves, we are comparing to the old value of hl and hr. We need to properly update hl and hr before proceeding to next step.
+
+One while loop.
+*/
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int i(0), j(n-1);
+        int sum(0);
+
+        int l = i, r = j;
+        while (i < j-1) {
+            int level = min(height[l], height[r]);
+            if (height[l] < height[r]) { //left side lower
+                i++;
+                if (height[i] < level) sum += level - height[i];
+                else l = i;
+            } else { //right side lower
+                j--;
+                if (height[j] < level) sum += level - height[j];
+                else r = j;
+            }
+        }
+        return sum;
+    }
+};
+
+
+/// Another solution using nested while loops.
 /// Two pointers on each end (l, r).
 /// Maintain the current minimum water level as level
 /// Scan from two sides until l and r meet, collect water amounts at each position
