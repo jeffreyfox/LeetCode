e3e769d995c6c6e7279da48d65f571ac637010a8
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-12-10 09:35:57 -0800

    070_climbing_stairs.cc

--- Algorithms/070_climbing_stairs.cc
+++ Algorithms/070_climbing_stairs.cc
@@ -1,3 +1,9 @@
+/*
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? 
+*/
+
 /// Fibonacci series, dynamic programming (bottom-up)
 class Solution {
 public:
@@ -10,3 +16,19 @@ public:
         return sol[n];    
     }
 };
+
+// Space optimized version. Only need value for the last 2 steps.
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) return n;
+        int curr = 0, last = 2, last_last = 1;
+        for(int i = 2; i < n; ++i) {
+            curr = last + last_last;
+            last_last = last;
+            last = curr;
+        }
+        return curr;
+    }
+};
+
