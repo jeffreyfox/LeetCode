dab86523d5500fd7abe1aca415f7ccf1335d1d52
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-12-12 14:37:44 -0800

    019_remove_nth_node_from_end.cc

--- Algorithms/019_remove_nth_node_from_end.cc
+++ Algorithms/019_remove_nth_node_from_end.cc
@@ -1,4 +1,17 @@
-/// Use dummy head node
+/*
+Given a linked list, remove the nth node from the end of list and return its head.
+
+For example,
+
+   Given linked list: 1->2->3->4->5, and n = 2.
+
+   After removing the second node from the end, the linked list becomes 1->2->3->5.
+
+Note:
+Given n will always be valid.
+Try to do this in one pass. 
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -7,21 +20,57 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+/// Solution 1. Use dummy head node. Two while loops.
+/// Two pointers, first and last. Let last move forward by n steps. And then left first and last move together until last reaches the tail.
+/// Then the node to be removed is first-next.
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode dum(0), *dummy(&dum);
+        ListNode *dummy = new ListNode(0);
         dummy->next = head;
-        ListNode *front(dummy), *back(dummy), *p(NULL);
-        for(int i = 0; i < n-1; ++i) front = front->next;
-        while(front->next) {
-            p = back;
-            front = front->next;
-            back = back->next;
+        ListNode *first(dummy), *last(dummy);
+        while(n) {
+            last = last->next;
+            n--;
         }
-        //delete back
-        p->next = back->next;
-        delete back;
-        return dummy->next;
+        while(last->next) {
+            first = first->next; last = last->next;
+        }
+        //delete first->next;
+        ListNode* tmp = first->next;
+        first->next = tmp->next;
+        delete tmp;
+        //return new head
+        head = dummy->next;
+        delete dummy;
+        return head;
     }
 };
+
+/// Solution 2. Same idea, but one pass (slightly simplified code).
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *first(dummy), *last(dummy);
+        while(last->next) {
+            last = last->next;
+            if(--n < 0) first = first->next;
+        }
+        
+        //delete first->next;
+        ListNode* tmp = first->next;
+        first->next = tmp->next;
+        delete tmp;
+        //return new head
+        head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
+
+
