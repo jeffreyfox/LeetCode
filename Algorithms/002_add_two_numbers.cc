ed7c256993081a411046d5c607ae41e62dc8366d
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-12-12 14:23:37 -0800

    002_add_two_numbers.cc

--- Algorithms/002_add_two_numbers.cc
+++ Algorithms/002_add_two_numbers.cc
@@ -8,24 +8,32 @@
  */
 
 // Use dummy head. Add additional node if final carry = 1.
+// Caveats:
+// 1. Don't forget to move l1 and l2 forward!
+// 2. Last step, scheck if carry > 0, not carry > 1 !
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if (l1 == NULL) return l2;
-        if (l2 == NULL) return l1;
-        ListNode dum(0), *p(&dum), *q(p);
-        int carry = 0;
+        ListNode* dummy = new ListNode(0), *p(dummy);
+        int sum = 0, carry = 0;
         while(l1 || l2) {
-            int v1 = (l1 == NULL) ? 0 : l1->val;
-            int v2 = (l2 == NULL) ? 0 : l2->val;
-            int sum = v1+v2+carry;
-            q->next = new ListNode(sum % 10);
+            int v1 = l1 ? l1->val : 0;
+            int v2 = l2 ? l2->val : 0;
+            sum = v1 + v2 + carry;
             carry = sum / 10;
-            if (l1) l1 = l1->next;
-            if (l2) l2 = l2->next;
-            q = q->next;
+            sum %= 10;
+            p->next = new ListNode(sum);
+            p = p->next;
+            if(l1) l1 = l1->next;
+            if(l2) l2 = l2->next;
         }
-        if (carry == 1) q->next = new ListNode(carry);
-        return p->next;
+        if(carry > 0) p->next = new ListNode(carry);
+        
+        p = dummy->next;
+        delete dummy;
+        return p;
     }
 };
+
+
