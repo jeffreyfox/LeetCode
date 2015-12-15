499996bf7c434f8ee36650d8656ff9d74bd7e0c5
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-12-14 21:44:15 -0800

    LCA

---
+++ Algorithms/235_lowest_common_ancestor_of_binary_search_tree.cc
@@ -0,0 +1,48 @@
+/*
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+
+        _______6______
+       /              \
+    ___2__          ___8__
+   /      \        /      \
+   0      _4       7       9
+         /  \
+         3   5
+
+For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// Take advantage of the property of BST. Do it recursively.
+// If root is same as either nodes, then root is LCA.
+// If the two nodes are one different sides of root, then root is LCA.
+// Otherwise recursively go to root's left or right subtree depending on which side of the root the nodes are at.
+// Time complexity is O(h), where h is the height of tree.
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || !p || !q) return NULL;
+        return LCA(root, p, q);
+    }
+    TreeNode* LCA(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return NULL;
+        if(p == root || q == root) return root; //one is equal to root
+        if((p->val < root->val) ^ (q->val < root->val)) return root; //lying on two sides of root
+        if(p->val < root->val) return LCA(root->left, p, q);
+        else return LCA(root->right, p, q);
+    }
+};
+
+
---
+++ Algorithms/236_lowest_common_ancestor_of_binary_tree.cc
@@ -0,0 +1,87 @@
+/*
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+
+        _______3______
+       /              \
+    ___5__          ___1__
+   /      \        /      \
+   6      _2       0       8
+         /  \
+         7   4
+
+For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// The tree is not BST, so we can't use value comparison. Instead, we need to examine both subtrees recursively O(n).
+
+// Solution 1. Use two auxiliary vectors storing the search paths from root to the node. 
+// Compare the two paths and find the last element both in two paths.
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || !p || !q) return NULL;
+        if( !search(root, p, path1) || !search(root, q, path2)) return NULL;
+        TreeNode* node = NULL;
+        while(!path1.empty() && !path2.empty()) {
+            TreeNode* n1 = path1.front(); path1.pop_front();
+            TreeNode* n2 = path2.front(); path2.pop_front();
+            if(n1 == n2) node = n1;
+            else break;
+        }
+        return node;
+    }
+    //search p under subtree root
+    bool search(TreeNode* root, TreeNode* p, deque<TreeNode*>& path) {
+        if(!root) return false;
+
+        path.push_back(root);
+        if(root == p) return true;
+        //search left and right subtree
+        if(search(root->left, p, path) || search(root->right, p, path)) return true;
+        path.pop_back(); //retrace
+        return false;
+    }
+    deque<TreeNode*> path1, path2;
+};
+
+// Solution 2 without the auxiliary path vector.
+// Assumes p and q are not NULL.
+// Similar to Solution 1 in that when root is equal to either node, return the root.
+// Then call the function for left and right subtrees. If both returns a non NULL value, that means one node is in left sub-tree, and the other is in the right sub-tree. In this case root is the LCA, and we return root.
+// In other cases, simply return the value returned by the recursive calls.
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return NULL;
+        if(root == p || root == q) return root;
+        TreeNode* lca_l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* lca_r = lowestCommonAncestor(root->right, p, q);
+        if(lca_l && lca_r) return root;
+        else return (lca_l ? lca_l : lca_r);
+    }
+};
+
