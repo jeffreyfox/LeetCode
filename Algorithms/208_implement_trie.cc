adcc80db11abdab7e473a3db4602415c9e396b2a
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-12-15 08:50:57 -0800

    208_implement_trie.cc

--- Algorithms/208_implement_trie.cc
+++ Algorithms/208_implement_trie.cc
@@ -1,4 +1,85 @@
-/// C++ version, use recursion.
+/*
+Implement a trie with insert, search, and startsWith methods.
+
+Note:
+You may assume that all inputs are consist of lowercase letters a-z. 
+*/
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie;
+// trie.insert("somestring");
+// trie.search("key");
+
+/// Solution 1, simpler, with delete function (which slows down runtime by 40 ms!)
+
+class TrieNode {
+public:
+    // Initialize your data structure here.
+    TrieNode() {
+        isKey = false;
+        links.resize(26, NULL);
+    }
+
+    bool isKey;
+    vector<TrieNode*> links;
+};
+
+class Trie {
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    ~Trie() {
+        eraseUtil(root);
+    }
+    // Inserts a word into the trie.
+    void insert(string word) {
+        insertUtil(root, word, 0);
+    }
+
+    // Returns if the word is in the trie.
+    bool search(string word) {
+        TrieNode* node = searchUtil(root, word, 0);
+        return node != NULL && node->isKey;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    bool startsWith(string prefix) {
+        TrieNode* node = searchUtil(root, prefix, 0);
+        return node != NULL;
+    }
+
+private:
+    //root can never be NULL
+    void insertUtil(TrieNode* root, const string& word, int i) {
+        if(i == word.size()) { root->isKey = true; return; }
+        int c = word[i] - 'a';
+        if(!root->links[c]) root->links[c] = new TrieNode();
+        insertUtil(root->links[c], word, i+1);
+    }
+    TrieNode* searchUtil(TrieNode* root, const string& word, int i) {
+        if(root == NULL) return NULL;
+        if(i == word.size()) return root;
+        int c = word[i] - 'a';
+        return searchUtil(root->links[c], word, i+1);
+    }
+    void eraseUtil(TrieNode* root) {
+        if(!root) return;
+        for(int c = 0; c < 26; ++c) eraseUtil(root->links[c]);
+        delete root;
+    }
+    TrieNode* root;
+};
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie;
+// trie.insert("somestring");
+// trie.search("key");
+
+
+/// Solution 2, slightly different.
 
 class TrieNode {
 public:
