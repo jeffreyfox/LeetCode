979efd45ad8fb7c68583c95da87bd2a41e7b0198
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-12-17 09:02:53 -0800

    some easy quesionts

--- Algorithms/165_compare_version_numbers.cc
+++ Algorithms/165_compare_version_numbers.cc
@@ -1,6 +1,21 @@
-// C++ version without using any system libraries
+/*
+Compare two version numbers version1 and version2.
+If version1 > version2 return 1, if version1 < version2 return -1, otherwise return 0.
+
+You may assume that the version strings are non-empty and contain only digits and the . character.
+The . character does not represent a decimal point and is used to separate number sequences.
+For instance, 2.5 is not "two and a half" or "half way to version three", it is the fifth second-level revision of the second first-level revision.
+
+Here is an example of version numbers ordering:
+
+0.1 < 1.1 < 1.2 < 13.37
+*/
+
+// C++ version without using any system libraries (iterative solution).
 // Use two pointers to scan two version numbers. Calculate integer value on the fly until encountering a '.' or end of string
-// remember to increment i and j after comparison
+// Caveats:
+// 1.1 > 1 (longer one is larger), but 1.0 = 1. Need to extract the next number for empty strings, which is set as zero.
+
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
@@ -21,6 +36,29 @@ public:
             else if (num1 < num2) return -1;
             i++; j++; //remember to increment i and j!
         }
-        return 0;
+        return 0; //equal
     }
 };
+
+// Another solution. Recursive.
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        return compareVersion(version1, version2, 0, 0);
+    }
+    int compareVersion(const string& version1, const string& version2, int i, int j) {
+        if(i >= version1.size() && j >= version2.size()) return 0; //termination
+        //now neither has reached end
+        int v1 = 0, v2 = 0;
+        while(i < version1.size() && version1[i] != '.') {
+            v1 = v1*10 + (version1[i++] - '0');
+        }
+        while(j < version2.size() && version2[j] != '.') {
+            v2 = v2*10 + (version2[j++] - '0');
+        }
+        if(v1 > v2) return 1;
+        else if(v1 < v2) return -1;
+        return compareVersion(version1, version2, i+1, j+1); //equal, compare next
+    }
+};
+
--- Algorithms/206_reverse_linked_list.cc
+++ Algorithms/206_reverse_linked_list.cc
@@ -1,3 +1,12 @@
+/*
+Reverse a singly linked list.
+
+click to show more hints.
+Hint:
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -7,16 +16,36 @@
  * };
  */
 
-// Recursive solution
+// Solution 1. Iterative solution. Use two pointers p and q. Temporary store q's next element.
+// remember to set head->next to NULL at last step!
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(head == NULL) return head; //for corner case
-        if(head->next == NULL) return head; // end of recursive calls
-        ListNode *next = head->next;
-        head->next = NULL; //set head's next as null
-        ListNode *newhead = reverseList(next);
-        next->next = head;
+        if(!head) return NULL;
+        ListNode* p = head, *q = head->next; //q goes beyond p
+        while(q) {
+            ListNode *next = q->next;
+            q->next = p;
+            p = q;
+            q = next;
+        }
+        head->next = NULL;
+        return p;
+    }
+};
+
+// Solution 2. Recursive solution. Set head's next pointer to NULL after calling reverse recursively for it's next. It's next will be assigned after traceback, when processing it's ancestor node.
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head) return NULL;
+        if(!head->next) return head;
+        ListNode *newhead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
         return newhead;
     }
 };
+
