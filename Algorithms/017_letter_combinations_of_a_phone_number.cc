2848e3cc0bbd7b5934177d033dccead7fbac8f83
jeffreyfox <jeffreyfox.chen@gmail.com>
2015-12-20 11:29:52 -0800

    017_letter_combinations_of_a_phone_number.cc

--- Algorithms/017_letter_combinations_of_a_phone_number.cc
+++ Algorithms/017_letter_combinations_of_a_phone_number.cc
@@ -1,43 +1,53 @@
-// Recursive dfs solution.
+/*
+Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+Note:
+Although the above answer is in lexicographical order, your answer could be in any order you want. 
+*/
+
+// Solution 1. Recursive dfs solution. Use a dict storing the conversion rule. Terminate at i == n.
+// A variant is also given where termination is at i == n-1.
 
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string> ret;
-        if(digits.empty()) return ret;
+        vector<string> dict = {"", "", "abc", "def", "ghi", "jkl", "mno", "prqs", "tuv", "wxyz"};
+        vector<string> result;
+        if(digits.empty()) return result;
         int n = digits.size();
-        string s(n, '0');
-        dfs(digits, 0, s, ret);
-        return ret;
+        string tmp(n, '0');
+        dfs(digits, 0, dict, tmp, result);
+        return result;
     }
-    
-    //generate letters for the number at digits[d]
-    void dfs(string& digits, int d, string& s, vector<string>& ret) {
-        if (d == digits.size()) {
-            ret.push_back(s); 
-            return; 
+    //starting from i-th digits
+    void dfs(const string& digits, int i, vector<string>& dict, string& tmp, vector<string>& result) {
+        if(i == digits.size()) {
+            result.push_back(tmp);
+            return;
         }
-        char c = digits[d];
-        for (int i = 0; i < numLetters(c); ++i) {
-            s[d] = letter(c, i);
-            dfs(digits, d+1, s, ret);
+        int idx = digits[i] - '0';
+        //choose among possible characters in dict[idx]
+        for(auto c : dict[idx]) {
+            tmp[i] = c;
+            dfs(digits, i+1, dict, tmp, result);
         }
     }
-    
-    int numLetters(char c) {
-        return (c == '7' || c == '9') ? 4 : 3;
+/*  // simplified dfs routine terminating at i == n-1
+    void dfs(const string& digits, int i, vector<string>& dict, string& tmp, vector<string>& result) {
+        int idx = digits[i] - '0';
+        int n = digits.size();
+        //choose among possible characters in dict[idx]
+        for(auto c : dict[idx]) {
+            tmp[i] = c;
+            if(i == n-1) result.push_back(tmp);
+            else dfs(digits, i+1, dict, tmp, result);
+        }
     }
-    
-    int letter(char c, int i) {
-        if (c == '2') return 'a' + i;
-        else if (c == '3') return 'd' + i;
-        else if (c == '4') return 'g' + i;
-        else if (c == '5') return 'j' + i;
-        else if (c == '6') return 'm' + i;
-        else if (c == '7') return 'p' + i;
-        else if (c == '8') return 't' + i;
-        else if (c == '9') return 'w' + i;
-        else return ' ';
-    }
-    
+*/
 };
+
