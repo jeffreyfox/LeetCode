33705d6b8b40f2b1f6baf3248be7e7734560cb3e
jeffreyfox <jeffreyfox.chen@gmail.com>
2016-01-08 09:14:02 -0800

    118_pascals_triangle.cc

--- Algorithms/118_pascals_triangle.cc
+++ Algorithms/118_pascals_triangle.cc
@@ -13,20 +13,36 @@ Return
 ]
 */
 
-// Straightforward solution. Commented unecessary checks for better efficiency
+// Straightforward solution. Commented unecessary checks for better efficiency. 0ms.
 
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int> > ret;
-        //if(numRows == 0) return ret;
-        for(int ir = 1; ir <= numRows; ++ir) {
-            ret.push_back(vector<int>(ir, 1));
-           // if(ir <= 2) continue;
-            for(int j = 1; j < ir-1; ++j) { //elements [1 .. ir-2], a total of ir-2
-                ret[ir-1][j] = ret[ir-2][j] + ret[ir-2][j-1];
-            }
+        vector<vector<int> > result;
+        result.resize(numRows);
+        for(int r = 0; r < numRows; ++r) {
+            int n = r+1; //number of elements in this row
+            result[r].resize(n, 1); //initialize as 1
+            for(int i = 1; i < n-1; ++i)  result[r][i] = result[r-1][i-1] + result[r-1][i];
         }
-        return ret;
+        return result;
     }
 };
+
+// Solution 2, with unecessary checks. 4ms.
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int> > result;
+        if(numRows == 0) return result;
+        result.resize(numRows);
+        result[0] = vector<int>(1, 1);
+        for(int r = 1; r < numRows; ++r) {
+            int n = r+1; //number of elements in this row
+            result[r].resize(n, 1); //initialize as 1
+            for(int i = 1; i < n-1; ++i)  result[r][i] = result[r-1][i-1] + result[r-1][i];
+        }
+        return result;
+    }
+};
+
