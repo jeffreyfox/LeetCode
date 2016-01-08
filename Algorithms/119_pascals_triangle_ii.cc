aa92a80ea97f4a87d4bae6cccce57812b343a21b
jeffreyfox <jeffreyfox.chen@gmail.com>
2016-01-08 09:27:09 -0800

    .

--- Algorithms/119_pascals_triangle_ii.cc
+++ Algorithms/119_pascals_triangle_ii.cc
@@ -8,7 +8,48 @@ Note:
 Could you optimize your algorithm to use only O(k) extra space? 
 */
 
-// DP solution using O(k) space
+// DP solution using only one vector. And avoided uncessary checks. Use an "old" variable to store the old value of current index, to be used for getting sum for the next element.
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> result(rowIndex+1, 1); //initialize as 1        
+        for(int r = 2; r <= rowIndex; ++r) {
+            int tmp = 0, old = 1;
+            for(int i = 1; i < r; ++i) {
+                tmp = result[i]; //store old value in temporary
+                result[i] += old;
+                old = tmp; //update old value
+            }
+        }
+        return result;
+    }
+};
+
+// Same as above. With additional unecessary checks.
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        if(rowIndex == 0) return vector<int>(1, 1);
+        if(rowIndex == 1) return vector<int>(2, 1);
+        vector<int> result(rowIndex+1, 0); //initialize as 0
+        result[0] = result[1] = 1; 
+
+        for(int r = 2; r <= rowIndex; ++r) {
+            int n = r + 1;
+            int tmp = 0, old = 1;
+            for(int i = 1; i < n-1; ++i) {
+                tmp = result[i]; //store old value in temporary
+                result[i] += old;
+                old = tmp; //update old value
+            }
+            result[n-1] = 1;
+        }
+        return result;
+    }
+};
+
+// DP solution using two vectors. swapping.
 
 class Solution {
 public:
