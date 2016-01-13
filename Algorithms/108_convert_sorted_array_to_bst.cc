c73c642d6a3410a45ed9de3a936be2001069ad2b
jeffreyfox <jeffreyfox.chen@gmail.com>
2016-01-13 09:07:05 -0800

    .

--- Algorithms/108_convert_sorted_array_to_bst.cc
+++ Algorithms/108_convert_sorted_array_to_bst.cc
@@ -31,3 +31,22 @@ public:
         return root;
     }
 };
+
+// A variant. Range is [beg, end)
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size());
+    }
+
+    TreeNode* helper(vector<int>& nums, int beg, int end) {
+        if(beg >= end) return NULL;
+        int mid = (beg+end)/2;
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = helper(nums, beg, mid);
+        root->right = helper(nums, mid+1, end);
+        return root;
+    }
+};
+
--- Algorithms/109_convert_sorted_list_to_bst.cc
+++ Algorithms/109_convert_sorted_list_to_bst.cc
@@ -21,28 +21,25 @@ Given a singly linked list where elements are sorted in ascending order, convert
  */
 
 // Recursive solution. First find middle element, then use that element as root of tree node, recursively build tree for the list segments before and after middle element, and attach as left and right child of root.
-// Caveat: corner case when middle element is head of list (when only 2 nodes in list), so left subtree does not exist! Need a check (middle == head) to avoid runtime error
 
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
-        if(head == NULL) return NULL;
-        if(head->next == NULL) return new TreeNode(head->val); //single node
-        ListNode *slow(head), *fast(head->next), *prev(NULL);
-        while(fast && fast->next) {
-            prev = slow;
+        return helper(head, NULL);
+    }
+    //construct from head (inclusive) to tail (exclusive)
+    TreeNode *helper(ListNode* head, ListNode *tail) {
+        if(head == tail) return NULL;
+        ListNode *slow = head, *fast = head;
+        while(fast->next != tail && fast->next->next != tail) { //this approach returns middle of element, or the first among the two middle ones.
             slow = slow->next;
             fast = fast->next->next;
         }
-        //use slow as root of new tree
+        //make slow the root
         TreeNode *root = new TreeNode(slow->val);
-        if(slow != head) { //build left child
-            prev->next = NULL; //temporary set as NULL
-            root->left = sortedListToBST(head);
-            prev->next = slow; //set it back
-        }
-        root->right = sortedListToBST(slow->next); //build right child
-        
+        root->left = helper(head, slow);
+        root->right = helper(slow->next, tail);
         return root;
     }
 };
+
