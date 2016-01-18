eb95fe3aa72295fea83f978fa6d2192b71954ec0
jeffreyfox <jeffreyfox.chen@gmail.com>
2016-01-18 09:29:10 -0800

    .

--- Algorithms/050_pow_x_n.cc
+++ Algorithms/050_pow_x_n.cc
@@ -1,27 +1,67 @@
-// O(lgn) solution, decode n into binary digits, and calculate x^1, x^2, x^4 procedingly. When the digit is set, add it into the factor.
+/*
+Implement pow(x, n)
+*/
+
+// O(lgn) iterative solution, decode n into binary digits, and calculate x^1, x^2, x^4 procedingly. When the digit is set, add it into the factor.
 // e.g:
 // n = 13 =   1     1    0     1
 //   x^13 = x^8 + x^4 + .. + x^1
-// Caveats:
-// 1. when n is negative, return 1/ val;
-// 2. when x is negative, return negative only when n is odd
-// 3. when abs(x) is 1, return 1 (or -1) directly
-// 4. maybe overflow?
+// 1. when n is negative, replace n by -n, and x by 1/x
+// 2. To avoid overflow, use long instead of int
+// 3. shift n towards right one by one.
 
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (n == 0) return 1;
-        int sign = (x < 0 && (n & 1)) ? -1 : 1;
-        x = abs(x);
-        if (x == 1.0) return sign*x;
-        bool pos = (n > 0);
-        double val = 1.0;
-        n = abs(n);
-        while (n) {
-            if (n & 1) val *= x;
-            n >>= 1; x *= x;
+        if(x == 0.0) return 0.0;
+        if(n == 0) return 1;
+        long m = n;
+        if(m < 0) {
+            m = -m; x = 1/x;
         }
-        return sign * (pos ? val : 1.0 / val);
+        //n > 0
+        double y = x;
+        double res = 1.0;
+        while(m) {
+            if(m & 1) res *= y;
+            m >>= 1;
+            y *= y;
+        }
+        return res;
     }
 };
+
+// Solution 2. Similar to above solution, except shift the power of two towards left (avoid overflow!).
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x == 0.0) return 0.0;
+        if(n == 0) return 1;
+        bool rev = (n < 0);
+        long nn = abs(n);
+        double result = 1.0;
+        long m = 1;
+        double y = x;
+        while(m <= nn) {
+            if(nn & m) result *= y;
+            y *= y; m <<= 1;
+        }
+        if(rev) result = 1.0/result;
+        return result;
+    }
+};
+
+// Solution 3. Recursive solution. Three cases: n is even, n is positive odd, n is negative odd.
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        if(n == -1) return 1/x;
+        double y = myPow(x, n/2);
+        if(n % 2 == 0) return y*y;
+        else if(n > 0) return y*y*x;
+        else return y*y/x;
+    }
+};
+
