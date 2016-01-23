adeb2b3eb1ee731c2a2a5678aaf229968d222042
jeffreyfox <jeffreyfox@users.noreply.github.com>
2016-01-23 11:39:48 -0800

    Update 062_unique_paths.cc

--- Algorithms/062_unique_paths.cc
+++ Algorithms/062_unique_paths.cc
@@ -24,3 +24,18 @@ public:
         return table[m-1][n-1];
     }
 };
+
+/// Space optimized solution, O(n) space using only one array
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        //table[j]: number of paths from board[0][0] to boad[i][j], i < m; j < n;
+        vector<int> table(n, 1);
+        for (int i = 1; i < m; ++i)
+            for (int j = 1; j < n; ++j)
+                table[j] += table[j-1];
+
+        return table[n-1];
+    }
+};
+
