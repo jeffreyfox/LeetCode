efb9445cbc3f56db5c3b115fc04517e4d9746bf3
jeffreyfox <jeffreyfox@users.noreply.github.com>
2016-01-23 11:33:53 -0800

    Update 064_minimum_path_sum.cc

--- Algorithms/064_minimum_path_sum.cc
+++ Algorithms/064_minimum_path_sum.cc
@@ -1,4 +1,30 @@
-// Dynamic programming, space-optimized version.
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+*/
+
+// Solution 1. Dynamic programming.
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if(grid.empty() || grid[0].empty()) return 0;
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int> > dp(m, vector<int>(n, 0));
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < m; ++i) dp[i][0] = dp[i-1][0] + grid[i][0];
+        for(int j = 1; j < n; ++j) dp[0][j] = dp[0][j-1] + grid[0][j];
+        for(int i = 1; i < m; ++i) {
+            for(int j = 1; j < n; ++j) {
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+
+// Solution 2. Dynamic programming, space-optimized version.
 // Be aware of initial values of the 1D array!
 
 class Solution {
