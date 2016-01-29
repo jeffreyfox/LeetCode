ce4cb0e8de86aac8290fbf29b0e8c6f25b4667ce
jeffreyfox <jeffreyfox@users.noreply.github.com>
2016-01-29 08:04:54 -0800

    Update 059_spiral_matrix_ii.cc

--- Algorithms/059_spiral_matrix_ii.cc
+++ Algorithms/059_spiral_matrix_ii.cc
@@ -1,42 +1,53 @@
+/*
+Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+
+For example,
+Given n = 3,
+
+You should return the following matrix:
+[
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+]
+*/
+
 // same idea as 54. spiral matrix 
 
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int> > matrix;
-        if (n <= 0) return matrix;
-        matrix.resize(n, vector<int>(n, 0));
-        int i(0), j(0);
-        int l(0), r(n-1), t(0), b(n-1);
-        int state = 0; //0: left, 1: down, 2: right, 3: up
-        int tot = n*n;
-        for (int k = 1; k <= tot; ++k) {
-            matrix[i][j] = k;
-            move(i, j, l, r, t, b, state);
+        vector<vector<int> > result;
+        if(n == 0) return result;
+        top = 0; bottom = n-1; left = 0; right = n-1;
+        state = 0;
+        result.resize(n, vector<int>(n, 0));
+        int i(0), j(0), k(1), nn(n*n);
+        while(k <= nn) {
+            result[i][j] = k++;
+            //update boundary and go to next step
+            switch(state) {
+                case 0:
+                    if(j < right) j++; // move right
+                    else { state = 1; i++; top++; } //start to move down
+                    break;
+                case 1:
+                    if(i < bottom) i++; // move down
+                    else { state = 2; j--; right--; } //start to move left
+                    break;
+                case 2:
+                    if(j > left) j--; //move left
+                    else { state = 3; i--; bottom--; } //start to move up
+                    break;
+                case 3:
+                    if(i > top) i--; //move up
+                    else { state = 0; j++; left++; } //start to move right 
+                    break;
+                default:
+                    break;
+            }
         }
-        return matrix;
+        return result;
     }
-    
-    void move(int& i, int& j, int& l, int& r, int& t, int& b, int& state) {
-        switch (state) {
-        case 0: //moving right
-            if (j < r) j++;
-            else { t++; i++; state = 1; } //change to down
-            break;
-        case 1: //moving down
-            if (i < b) i++;
-            else { r--; j--; state = 2; } //change to left
-            break;
-        case 2: //moving left
-            if (j > l) j--;
-            else { b--; i--; state = 3; } //change to up
-            break;
-        case 3: //moving up
-            if (i > t) i--;
-            else { l++; j++; state = 0; } //change to left
-            break;
-        default:
-            break;
-        }
-    }
+    int top, bottom, left, right, state;
 };
