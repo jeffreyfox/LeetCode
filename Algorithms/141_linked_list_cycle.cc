b4dde6e52a93eeb037c082591b19cd15c6b7c6e9
jeffreyfox <jeffreyfox@users.noreply.github.com>
2016-01-28 20:57:31 -0800

    Update 141_linked_list_cycle.cc

--- Algorithms/141_linked_list_cycle.cc
+++ Algorithms/141_linked_list_cycle.cc
@@ -1,3 +1,10 @@
+/*
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -7,7 +14,8 @@
  * };
  */
 
-// maintain a slow and a fast pointer
+// Floyd's algorithm. Maintain a slow and a fast pointer
+// see http://keithschwarz.com/interesting/code/?dir=find-duplicate
 
 class Solution {
 public:
