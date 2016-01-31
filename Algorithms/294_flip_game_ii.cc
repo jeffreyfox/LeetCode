b90379679a47a2d9b20268fc785d620cce09857e
jeffreyfox <jeffreyfox@users.noreply.github.com>
2016-01-30 18:10:20 -0800

    Update 294_flip_game_ii.cc

--- Algorithms/294_flip_game_ii.cc
+++ Algorithms/294_flip_game_ii.cc
@@ -31,3 +31,31 @@ public:
         return false;
     }
 };
+
+// Solution 2. Dynamic programming. Store previous solutions in an unordered_map
+
+class Solution {
+public:
+    bool canWin(string s) {
+        if(s.empty()) return false;
+        if(s.size() == 1) return false;
+        return canWinUtil(s);
+    }
+    bool canWinUtil(string& s) {
+        if(dp.count(s)) return dp[s];
+        bool wins = false;
+        int n = s.size();
+        for(int i = 0; i < n-1; i++) { //try all possibilities
+            if(s[i] == '+' && s[i+1] == '+') {
+                s[i] = s[i+1] = '-';
+                wins = !canWinUtil(s);
+                s[i] = s[i+1] = '+'; //retrace
+                if(wins) break;
+            }
+        }
+        dp[s] = wins;
+        return wins;
+    }
+    unordered_map<string, bool> dp;
+};
+
