3c520d26208794910957e92e71b0f4f3fd8daffa
jeffreyfox <jeffreyfox@users.noreply.github.com>
2016-01-30 20:32:05 -0800

    Update 201_bitwise_and_of_numbers_range.cc

--- Algorithms/201_bitwise_and_of_numbers_range.cc
+++ Algorithms/201_bitwise_and_of_numbers_range.cc
@@ -1,3 +1,10 @@
+/*
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+For example, given the range [5, 7], you should return 4.
+*/
+
+
 /* If we search from highest bits to lowest bits, the result should have the same bit if m and n have the same bit, 
 and once we encountered the first different bit, then all the bits in result on and after this bit should be zero. 
 The reason is as follows. Suppose the first different bit is at i, then m[i] should be 0 and n[i] should be 1 (m <=n), 
@@ -18,5 +25,38 @@ public:
         }
         //after loop, m_xor is 0, and mask is of form 00111..
         return m_and & (~mask);
+    }
+};
+
+// Solution 2. Shift m and n until they are equal. Record the number of shifts, and shift m back.
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int i = 0;
+        while(m != n) {
+            i ++;
+            m >>= 1;
+            n >>= 1;
+        }
+        return m << i;
+    }
+};
+
+// Solution 3.
+// Alternative solution by shifting bits. For explanations refer to the first solution.
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        if(m == n || n == 0) return n;
+        int ret = 0;
+        unsigned int i = 0x80000000; //1000...
+        while(i) {
+            if ((m ^ n) & i) break; //m and n's bits differ at that bit
+            else ret = ret | (m & i); //bit the same, copy m's bit to result
+            i >>= 1; //move one bit lower
+        }
+        return ret;
     }
 };
