ac59ac1e87fbf0f6f02caa086aef70b1fa4e4d2d
jeffreyfox <jeffreyfox@users.noreply.github.com>
2016-01-31 09:44:37 -0800

    Update 199_binary_tree_right_side_view.cc

--- Algorithms/199_binary_tree_right_side_view.cc
+++ Algorithms/199_binary_tree_right_side_view.cc
@@ -1,3 +1,16 @@
+/*
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+You should return [1, 3, 4].
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -8,7 +21,31 @@
  * };
  */
  
- /// Level order traversal. We encounter the rightmost element at each level in two cases:
+ /// Solution 1. Level order traversal using two vectors (curr, next). When getting the last element of curr, insert it to result.
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        if(!root) return result;
+        vector<TreeNode*> curr, next;
+        curr.push_back(root);
+        while(!curr.empty()) {
+            int n = curr.size();
+            for(int i = 0; i < n; i++) {
+                TreeNode* node = curr[i];
+                if(node->left)  next.push_back(node->left);
+                if(node->right) next.push_back(node->right);
+                if(i == n-1) result.push_back(node->val);
+            }
+            curr.swap(next);
+            next.clear();
+        }
+        return result;
+    }
+};
+
+ /// Solution 2. Level order traversal. We encounter the rightmost element at each level in two cases:
  /// (a) there's no element in the queue (queue empty)
  /// (b) the next element in the queue has higher level
  
