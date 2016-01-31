11b651f92f07de3be90ab301e1c47234d2c8c298
jeffreyfox <jeffreyfox@users.noreply.github.com>
2016-01-31 10:07:40 -0800

    Update 137_single_number_ii.cc

--- Algorithms/137_single_number_ii.cc
+++ Algorithms/137_single_number_ii.cc
@@ -26,8 +26,30 @@ public:
         for(int i = 0; i < nums.size(); ++i) {
             threes = twos & nums[i];
             twos = (twos | (ones & nums[i])) & ~threes;
-            ones = (ones | nums[i]) & ~threes;
+            ones = (ones | nums[i]) & ~threes; // it is |, not ^
         }
         return ones;
+    }
+};
+
+// Extension to general cases (N times)
+/// Use an array to store the occurrences of each bit: 
+/// count[1] stores whether the bit has been set for AT LEAST 1 times
+/// count[2] stores whether the bit has been set for AT LEAST 2 times
+/// count[N] stores whether the bit has been set for N times
+/// when each bit position has reached maximum number of times (N), we reset the corresponding bit positions in count to zero
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        const int N = 3; //number of times, can be generalized to > 3
+        vector<int> count(N+1, 0); //count[i]: occured AT LEAST i times? (count[0] unused)
+        for(int i = 0; i < nums.size(); ++i) {
+            count[N] = count[N-1] & nums[i];
+            for(int n = N-1; n > 1; --n)
+                count[n] = (count[n] | (count[n-1] & nums[i])) & ~count[N];
+            count[1] = (count[1] | nums[i]) & ~count[N];
+        }
+        return count[1];
     }
 };
