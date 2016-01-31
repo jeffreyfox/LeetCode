4f055cc6d801f2b6710d818ad017c5d89f946046
jeffreyfox <jeffreyfox@users.noreply.github.com>
2016-01-31 08:41:42 -0800

    Update 105_construct_binary_tree_from_preorer_and_inorder.cc

--- Algorithms/105_construct_binary_tree_from_preorer_and_inorder.cc
+++ Algorithms/105_construct_binary_tree_from_preorer_and_inorder.cc
@@ -39,3 +39,31 @@ public:
         return root;
     }
 };
+
+// Solution 2, slightly different version
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if(preorder.empty()) return NULL;
+        int n = preorder.size();
+        return buildTree(preorder, 0, n-1, inorder, 0, n-1);
+    }
+    //build tree from pre[p1 ..p2] and in[i1 .. i2]
+    TreeNode* buildTree(vector<int>& preorder, int p1, int p2, vector<int>& inorder, int i1, int i2) {
+        int len = p2 - p1 + 1;
+        if(len <= 0) return NULL;
+        if(len == 1) return new TreeNode(preorder[p1]); //one node only
+        int root_val = preorder[p1];
+        TreeNode *root = new TreeNode(root_val);
+        
+        int i = i1;
+        for(; i <= i2; i++) {
+            if(inorder[i] == root_val) break;
+        }
+        //i is the root in inorder
+        int nl = i-i1; //number of elements in left subtree
+        root->left = buildTree(preorder, p1+1, p1+nl, inorder, i1, i-1);
+        root->right = buildTree(preorder, p1+nl+1, p2, inorder, i+1, i2);
+    }
+};
