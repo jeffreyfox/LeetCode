957c372510a0130f727afe9c2475f3847f8c9890
jeffreyfox <jeffreyfox@users.noreply.github.com>
2016-02-14 09:25:39 -0800

    Update 086_partition_list.cc

--- Algorithms/086_partition_list.cc
+++ Algorithms/086_partition_list.cc
@@ -1,3 +1,13 @@
+/*
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+For example,
+Given 1->4->3->2->5->2 and x = 3,
+return 1->2->2->4->3->5.
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -7,25 +17,28 @@
  * };
  */
 
-// Use two dummy heads for sublist < x and sublist >=x
-// when finish, patch two lists together
+// No dummy heads, two sub-lists for < x and >= x
+// when finish, patch two lists together, and remember to set list tail's next pointer to NULL!
 
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-       if (head == NULL) return head;
-       ListNode dum1(0), dum2(0), *p1(&dum1), *p2(&dum2);
-       ListNode *q1(p1), *q2(p2);
-       ListNode *curr = head;
-       while(curr) {
-           if(curr->val < x) { q1->next = curr; q1 = q1->next; }
-           else { q2->next = curr; q2 = q2->next; }
-           curr = curr->next;
-       }
-       if(p1->next == NULL) return p2->next;
-       q1->next = p2->next;
-       if(p2->next) q2->next = NULL;
-       return p1->next;
+        if(!head || !head->next) return head;
+        //p1 .. q1 < x, p2 .. q2 >= x
+        ListNode *p1(NULL), *q1(NULL), *p2(NULL), *q2(NULL);
+        ListNode *curr = head;
+        while(curr) {
+            if(curr->val < x) {
+                if(!p1) p1 = q1 = curr; //first one
+                else q1 = q1->next = curr; 
+            } else {
+                if(!p2) p2 = q2 = curr; //first one
+                else q2 = q2->next = curr; 
+            }
+            curr = curr->next;
+        }
+        if(q1) q1->next = p2; //connect the two
+        if(q2) q2->next = NULL; //set tail to NULL!
+        return p1 ? p1 : p2;
     }
-
 };
