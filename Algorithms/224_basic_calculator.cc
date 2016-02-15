9d117a1b8d441f152004858394c0cccf836eaae8
jeffreyfox <jeffreyfox.chen@gmail.com>
2016-02-15 11:51:27 -0800

    224_basic_calculator.cc

--- Algorithms/224_basic_calculator.cc
+++ Algorithms/224_basic_calculator.cc
@@ -1,4 +1,20 @@
-// Basic calculator, with "+-" and parentheses, no "*/"
+/*
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+
+You may assume that the given expression is always valid.
+
+Some examples:
+
+"1 + 1" = 2
+" 2-1 + 2 " = 3
+"(1+(4+5+2)-3)+(6+8)" = 23
+
+Note: Do not use the eval built-in library function. 
+*/
+
+// Solution 1. Basic calculator, with "+-" and parentheses, no "*/"
 // Two stacks, one for number and one for operators (can use one, where operator can be stored by an integer sign)
 // 1. when seeing a digit, update number
 // 2. when seeing a + or -, update result
@@ -38,3 +54,32 @@ public:
     }
 
 };
+
+// Solution 2. Use only one stack, push sign (1/-1) and value to the same stack, when pop, pop twice
+
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> st;
+        int res = 0, num = 0, sgn = 1;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] >= '0' && s[i] <= '9') num = num*10 + (s[i] - '0');
+            else if (s[i] == '+' || s[i] == '-') {
+                res += sgn*num;
+                sgn = (s[i] == '+') ? 1 : -1;
+                num = 0; //reset num
+            } else if (s[i] == '(') {
+                st.push(res); st.push(sgn);
+                num = 0; res = 0; sgn = 1; //reset
+            } else if (s[i] == ')') {
+                res += sgn*num; //add last number first
+                res *= st.top(); st.pop(); //pop out sign
+                res += st.top(); st.pop(); //pop out number
+                num = 0; //reset num
+            }
+        }
+        if (num != 0) res += sgn*num;
+        return res;
+    }
+};
+
