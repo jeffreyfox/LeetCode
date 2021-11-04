647e5381143fd7702a732eaee0c5259def1c999c
jeffreyfox <jeffreyfox@users.noreply.github.com>
2021-11-03 22:28:02 -0700

    Update 021_merge_two_sorted_lists.cc

--- Algorithms/021_merge_two_sorted_lists.cc
+++ Algorithms/021_merge_two_sorted_lists.cc
@@ -32,3 +32,31 @@ public:
         return p->next;
     }
 };
+
+// 2021.11
+// No tail pointer. One pass.
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode tmp(0);
+        ListNode *dummy(&tmp);
+        ListNode *p = dummy;
+        while (l1 || l2) {
+            if (l1 == nullptr) {
+                p->next = new ListNode(l2->val);
+                l2 = l2->next;
+            } else if (l2 == nullptr) {
+                p->next = new ListNode(l1->val);
+                l1 = l1->next;
+            } else if (l1->val < l2->val) {
+                p->next = new ListNode(l1->val);
+                l1 = l1->next;
+            } else {
+                p->next = new ListNode(l2->val);                
+                l2 = l2->next;
+            }
+            p = p->next;
+        }
+        return dummy->next;
+    }
+};
