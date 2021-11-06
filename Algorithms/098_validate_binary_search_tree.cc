4aa6a6335dbfa191369c386967a2746089960717
jeffreyfox <jeffreyfox@users.noreply.github.com>
2021-11-06 09:59:07 -0700

    Update 098_validate_binary_search_tree.cc

--- Algorithms/098_validate_binary_search_tree.cc
+++ Algorithms/098_validate_binary_search_tree.cc
@@ -37,3 +37,19 @@ public:
         return validate(root->left, lo, x) && validate(root->right, x, hi);
     }
 };
+
+// 2021. Uses two booleans instead of long bounds
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValidHelper(root, 0, 0, false, false);
+    }
+    
+    bool isValidHelper(TreeNode * root, int lo, int hi, bool lo_initialized, bool hi_initialized) {
+        if (root == nullptr) return true;
+        if (lo_initialized && root->val <= lo) return false;
+        if (hi_initialized && root->val >= hi) return false;        
+        // Recurse on the children
+        return isValidHelper(root->left, lo, root->val, lo_initialized, true) && isValidHelper(root->right, root->val, hi, true, hi_initialized);
+    }
+};
