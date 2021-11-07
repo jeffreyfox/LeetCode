ab14407facd8854fcf0b51687be7aa6c9cc51076
jeffreyfox <jeffreyfox@users.noreply.github.com>
2021-11-06 22:54:45 -0700

    Update 141_linked_list_cycle.cc

--- Algorithms/141_linked_list_cycle.cc
+++ Algorithms/141_linked_list_cycle.cc
@@ -14,6 +14,22 @@ Can you solve it without using extra space?
  * };
  */
 
+// 2021. Use a hashtable to store the address of already visited nodes.
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head) return false;
+        unordered_set<ListNode*> dict;
+        while (head) {
+            if (dict.count(head)) return true;
+            dict.insert(head);
+            head = head->next;
+        }
+        return false;
+    }
+};
+
+// 2015.
 // Floyd's algorithm. Maintain a slow and a fast pointer
 // see http://keithschwarz.com/interesting/code/?dir=find-duplicate
 
