64aedc7a9a6b03acd7ce426287a1a17cc7db1591
jeffreyfox <jeffreyfox@users.noreply.github.com>
2021-11-22 09:25:58 -0800

    Update 286_walls_and_gates.cc

--- Algorithms/286_walls_and_gates.cc
+++ Algorithms/286_walls_and_gates.cc
@@ -17,7 +17,52 @@ After running your function, the 2D grid should be:
   1  -1   2  -1
   0  -1   3   4
 */
-  
+
+// 2021.
+// Standard multi-source BFS using a queue.
+class Solution {
+public:    
+    const int kInf = 2147483647;
+    using coord = pair<int, int>;
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        deque<coord> q;
+        int m = rooms.size(), n = rooms[0].size();
+        // First run push gates to queue
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int val = rooms[i][j];                
+                if (val == 0) q.push_back(make_pair(i, j));
+            }
+        }
+        
+        // Next do BFS starting the gates
+        while (!q.empty()) {
+            const auto v = q.front(); q.pop_front();
+            const int i = v.first, j = v.second;
+            const int distance = rooms[i][j] + 1;
+            if (ShouldVisit(rooms, m, n, i+1, j)) {
+                rooms[i+1][j] = distance; q.push_back(make_pair(i+1, j));
+            }
+            if (ShouldVisit(rooms, m, n, i-1, j)) {
+                rooms[i-1][j] = distance; q.push_back(make_pair(i-1, j));
+            }
+            if (ShouldVisit(rooms, m, n, i, j+1)) {
+                rooms[i][j+1] = distance; q.push_back(make_pair(i, j+1));
+            }
+            if (ShouldVisit(rooms, m, n, i, j-1)) {
+                rooms[i][j-1] = distance; q.push_back(make_pair(i, j-1));
+            }
+        }
+    }
+    
+    // Returns true if we should visit this room i, j
+    bool ShouldVisit(vector<vector<int>>& rooms, int m, int n, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n) return false;
+        return (rooms[i][j] == kInf);
+    }
+};
+
+// 2015.
 // Standard multi-source BFS. First identify all sources, then BFS using two vectors.
 
 class Solution {
