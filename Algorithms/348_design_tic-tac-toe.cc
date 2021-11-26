3f1a25772497def46a1624c88907f8e42831d0be
jeffreyfox <jeffreyfox@users.noreply.github.com>
2021-11-26 08:50:45 -0800

    Create 348_design_tic-tac-toe.cc

---
+++ Algorithms/348_design_tic-tac-toe.cc
@@ -0,0 +1,103 @@
+/*
+Assume the following rules are for the tic-tac-toe game on an n x n board between two players:
+
+A move is guaranteed to be valid and is placed on an empty block.
+Once a winning condition is reached, no more moves are allowed.
+A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+Implement the TicTacToe class:
+
+TicTacToe(int n) Initializes the object the size of the board n.
+int move(int row, int col, int player) Indicates that the player with id player plays at the cell (row, col) of the board. The move is guaranteed to be a valid move.
+ 
+
+Example 1:
+
+Input
+["TicTacToe", "move", "move", "move", "move", "move", "move", "move"]
+[[3], [0, 0, 1], [0, 2, 2], [2, 2, 1], [1, 1, 2], [2, 0, 1], [1, 0, 2], [2, 1, 1]]
+Output
+[null, 0, 0, 0, 0, 0, 0, 1]
+
+Explanation
+TicTacToe ticTacToe = new TicTacToe(3);
+Assume that player 1 is "X" and player 2 is "O" in the board.
+ticTacToe.move(0, 0, 1); // return 0 (no one wins)
+|X| | |
+| | | |    // Player 1 makes a move at (0, 0).
+| | | |
+
+ticTacToe.move(0, 2, 2); // return 0 (no one wins)
+|X| |O|
+| | | |    // Player 2 makes a move at (0, 2).
+| | | |
+
+ticTacToe.move(2, 2, 1); // return 0 (no one wins)
+|X| |O|
+| | | |    // Player 1 makes a move at (2, 2).
+| | |X|
+
+ticTacToe.move(1, 1, 2); // return 0 (no one wins)
+|X| |O|
+| |O| |    // Player 2 makes a move at (1, 1).
+| | |X|
+
+ticTacToe.move(2, 0, 1); // return 0 (no one wins)
+|X| |O|
+| |O| |    // Player 1 makes a move at (2, 0).
+|X| |X|
+
+ticTacToe.move(1, 0, 2); // return 0 (no one wins)
+|X| |O|
+|O|O| |    // Player 2 makes a move at (1, 0).
+|X| |X|
+
+ticTacToe.move(2, 1, 1); // return 1 (player 1 wins)
+|X| |O|
+|O|O| |    // Player 1 makes a move at (2, 1).
+|X|X|X|
+ 
+
+Constraints:
+
+2 <= n <= 100
+player is 1 or 2.
+0 <= row, col < n
+(row, col) are unique for each different call to move.
+At most n2 calls will be made to move.
+*/
+
+/*
+Keep track of the mark counts for both players on each row, col, diagonal and anti-diagonal.
+When a player makes a move, increment the counter and also check if the value is equal to n. If yes the player has won.
+Can further optimize to store a number which equals ‘X’ counts - ‘O’ counts (one player is 1 the other is -1). When the count is n or -n the player has won.
+*/
+class TicTacToe {
+public:
+    TicTacToe(int n) {
+        this->n = n;
+        row_counts.resize(n, vector<int>(2, 0));
+        col_counts.resize(n, vector<int>(2, 0));
+        diag_counts.resize(2, vector<int>(2, 0));
+    }
+    
+    int move(int row, int col, int player) {
+        if (++row_counts[row][player-1] == n) return player;
+        if (++col_counts[col][player-1] == n) return player;
+        if (row == col) {
+            if (++diag_counts[0][player-1] == n) return player;
+        }
+        if (row + col == n - 1) {
+            if (++diag_counts[1][player-1] == n) return player;
+        }
+        return 0;
+    }
+private:
+    vector<vector<int>> row_counts, col_counts, diag_counts;
+    int n;
+};
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe* obj = new TicTacToe(n);
+ * int param_1 = obj->move(row,col,player);
+ */
