0635898400d907f67a9c1bd457caa6a95ba20b7e
jeffreyfox <jeffreyfox@users.noreply.github.com>
2021-12-10 08:59:08 -0800

    Update 037_sudoku_solver.cc

--- Algorithms/037_sudoku_solver.cc
+++ Algorithms/037_sudoku_solver.cc
@@ -7,55 +7,65 @@ You may assume that there will be only one unique solution.
 */
 
 /*
-DFS backtracking. Using a helper function canPlace (44ms).
-Two passes: first pass initializes state, second pass do dfs and fills empty cells.
+DFS backtracking. Using a helper function canPlace.
 
 Have dfs return true or false so that once a solution is find, terminate search immediately.
 
-Caveats:
-1. numbers start from 1, not 0.
-2. when to terminate? i == 9, not j == 9!. 
+We use an iterative function to find the next unfilled position instead of recursively to reduce stack size.
 */
 
 class Solution {
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        dfs(board, 0, 0);
+        dfs(0, 0, board);                
     }
-    bool dfs(vector<vector<char> >& board, int i, int j) {
-        //try all possible choices
-        if(i == 9) return true;
-        int inext = (j == 8) ? i+1 : i;
-        int jnext = (j == 8) ? 0 : j+1;
-        //already filled, just jump to next
-        if(board[i][j] != '.') return dfs(board, inext, jnext);
-        //only try when board[i][j] is '.'
-        for(char c = '1'; c <= '9'; c++) {
-            if(canPlace(board, i, j, c)) {
-                board[i][j] = c;
-                if(dfs(board, inext, jnext)) return true;
-            }
-            board[i][j] = '.'; //reset back to '.'
+    
+    bool dfs(int row, int col, vector<vector<char>>& board) {
+        if (row == 9) return true;
+        if (board[row][col] != '.') {
+            auto next = FindNext(row, col, board);
+            return dfs(next.first, next.second, board);
         }
-        return false; //tried all possibilities
+        for (char c = '1'; c <= '9'; ++c) {
+            if (canPlace(row, col, c, board)) {
+                board[row][col] = c;
+                auto next = FindNext(row, col, board);
+                if (dfs(next.first, next.second, board)) return true;
+                // backtrack
+                board[row][col] = '.';
+            }
+        }
+        return false;
     }
-
-    bool canPlace(vector<vector<char> >& board, int i, int j, char c) {
-        for(int irow = 0; irow < 9; irow++)
-            if(irow != i && board[irow][j] == c) return false;
-        
-        for(int jcol = 0; jcol < 9; jcol++)    
-            if(jcol != j && board[i][jcol] == c) return false;
-        
-        int iblk = i-i%3, jblk = j-j%3; //block offset
+    
+    pair<int, int> FindNext(int row, int col, const vector<vector<char>>& board) {
+        while (row < 9 && col < 9) {
+            if (col < 8) col++;
+            else { row++; col = 0;}
+            if (row == 9) return {row, col};
+            if (board[row][col] == '.') break;
+        }
+        return make_pair(row, col);
+    }
+    
+    bool canPlace(int row, int col, char c, const vector<vector<char>>& board) {
+        // check row
+        for (int jcol = 0; jcol < 9; ++jcol) {
+            if (board[row][jcol] == c) return false;
+        }
+        // check col
+        for (int irow = 0; irow < 9; ++irow) {
+            if (board[irow][col] == c) return false;
+        }
+        // check block
+        int iblk = row/3, jblk = col/3; //block offset
         for(int l = 0; l < 9; l++) { // try all entries within the block
-            int irow = iblk + l/3, jcol = jblk + l%3;
-            if(irow != i && jcol != j && board[irow][jcol] == c) return false;
+            int irow = iblk*3 + l/3, jcol = jblk*3 + l%3;
+            if(board[irow][jcol] == c) return false;
         }
         return true;
     }
 };
-
 /*
 Solution 2. Use three bits vectors indicating states for rows, colums, and blocks (8ms).
 
