75b81f0fdc191b73ab7082f46ce613bcb82401df
jeffreyfox <jeffreyfox@users.noreply.github.com>
2021-12-11 21:38:42 -0800

    Update 251_flatten_2d_vector.cc

--- Algorithms/251_flatten_2d_vector.cc
+++ Algorithms/251_flatten_2d_vector.cc
@@ -1,69 +1,84 @@
 /*
-Implement an iterator to flatten a 2d vector.
+Design an iterator to flatten a 2D vector. It should support the next and hasNext operations.
 
-For example,
-Given 2d vector =
+Implement the Vector2D class:
 
-[
-  [1,2],
-  [3],
-  [4,5,6]
-]
+Vector2D(int[][] vec) initializes the object with the 2D vector vec.
+next() returns the next element from the 2D vector and moves the pointer one step forward. You may assume that all the calls to next are valid.
+hasNext() returns true if there are still some elements in the vector, and false otherwise.
+ 
 
-By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,2,3,4,5,6].
+Example 1:
 
-Hint:
+Input
+["Vector2D", "next", "next", "next", "hasNext", "hasNext", "next", "hasNext"]
+[[[[1, 2], [3], [4]]], [], [], [], [], [], [], []]
+Output
+[null, 1, 2, 3, true, true, 4, false]
 
-    How many variables do you need to keep track?
-    Two variables is all you need. Try with x and y.
-    Beware of empty rows. It could be the first few rows.
-    To write correct code, think about the invariant to maintain. What is it?
-    The invariant is x and y must always point to a valid point in the 2d vector. Should you maintain your invariant ahead of time or right when you need it?
-    Not sure? Think about how you would implement hasNext(). Which is more complex?
-    Common logic in two different places should be refactored into a common method.
+Explanation
+Vector2D vector2D = new Vector2D([[1, 2], [3], [4]]);
+vector2D.next();    // return 1
+vector2D.next();    // return 2
+vector2D.next();    // return 3
+vector2D.hasNext(); // return True
+vector2D.hasNext(); // return True
+vector2D.next();    // return 4
+vector2D.hasNext(); // return False
+ 
 
-Follow up:
-As an added challenge, try to code it using only iterators in C++ or iterators in Java. 
+Constraints:
+
+0 <= vec.length <= 200
+0 <= vec[i].length <= 500
+-500 <= vec[i][j] <= 500
+At most 105 calls will be made to next and hasNext.
+ 
+
+Follow up: As an added challenge, try to code it using only iterators in C++ or iterators in Java.
 */
+ 
+// Solution 1 using two pointers: i is the outer pointer and j is the inner pointer. When calling next() and hasNext(), we call the function moveToNext(). This function
+// moves the cursors to the next element if needed.
+// - If j already points to the next element, then no op.
+// - Otherwise, j must be pointing to the end of vec[i], we then move down to the next rows until we find a non-empty row or read the end.
+class Vector2D {
+public:
+    Vector2D(vector<vector<int>>& vec) : v(vec), i(0), j(0) {        
+    }
+    
+    int next() {
+        moveToNext();
+        return v[i][j++];
+    }
+    
+    bool hasNext() {
+        moveToNext();        
+        return i < v.size();
+    }
+    
+    // Move the cursor to the next element.
+    // If j already points to the next element, then no op.
+    // Otherwise, j must be pointing to the end of vec[i], we then move down to
+    // the next rows until we find a non-empty row or read the end.
+    void moveToNext() {
+        while (i < v.size() && j == v[i].size()) {
+            i++;
+            j = 0;
+        }
+    }
+    
+private:
+    vector<vector<int>> &v;
+    int i, j;
+};
 
 /**
  * Your Vector2D object will be instantiated and called as such:
- * Vector2D i(vec2d);
- * while (i.hasNext()) cout << i.next();
+ * Vector2D* obj = new Vector2D(vec);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
  */
-
-class Vector2D {
-public:
-    Vector2D(vector<vector<int>>& vec2d) {
-        nums = vec2d;
-        n = nums.size();
-        index = 0;
-        arrayIdx = 0;
-        while(arrayIdx < n && nums[arrayIdx].empty()) arrayIdx++;
-    }
-
-    int next() {
-        if(arrayIdx == n) return -1;
-        int val = nums[arrayIdx][index];
-        index++;
-        if(index == nums[arrayIdx].size()) {
-            index = 0;
-            arrayIdx ++;
-            while(arrayIdx < n && nums[arrayIdx].empty()) arrayIdx ++;
-        }
-        return val;
-    }
-
-    bool hasNext() {
-        return arrayIdx < n;
-    }
-
-private:
-    vector<vector<int> > nums;
-    int arrayIdx; //current array index
-    int index; //index within current array
-    int n; //number of arrays
-};
 
 // Solution 2. Similar to 281. Use a stack to store the pair of iterators
 class Vector2D {
