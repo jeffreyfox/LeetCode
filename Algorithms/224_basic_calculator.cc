7a12d9b987e0ec969c5a2eacb350602a2ef8cbc3
jeffreyfox <jeffreyfox@users.noreply.github.com>
2021-12-13 16:09:17 -0800

    Update 224_basic_calculator.cc

--- Algorithms/224_basic_calculator.cc
+++ Algorithms/224_basic_calculator.cc
@@ -14,6 +14,47 @@ Some examples:
 Note: Do not use the eval built-in library function. 
 */
 
+// 2021. Elegant solution from https://www.youtube.com/watch?v=081AqOuasw0.
+// Use a stack.
+// We can use a variable to track the sign (1 or -1).
+// When there are no brackets, we can compute results on the fly and no stack is needed. We keep track of rolling sum and the sign.
+// Initially, sum = 0, sign = 1. When we see a number, add it to the sum (apply the sign seen one step before).
+/// When there are brackets, we need a stack to store the sum and the sign, e.g. 2-(3+4). When see the left bracket '(', we need to save 2 and - to the stack and
+// compute 3+4 on the fly.
+// When we see a right bracket ')', we reach the end of this level. The current sum 7 needs to be applied to the operand in the stack. Here we have sum = 7 and
+// 2 and - in the stack. We pop the two and apply them to 7 and get -5.
+
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> st;
+        int sign = 1, sum = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ' ') continue;
+            if (s[i] == '+') sign = 1;
+            else if (s[i] == '-') sign = -1;
+            else if (s[i] >= '0' && s[i] <= '9') {
+                int num = 0;
+                while (i < s.size() && s[i] >= '0' && s[i] <= '9') {
+                    num = 10*num + (s[i] - '0');
+                    i++;
+                }
+                sum += sign * num;
+                i--;
+            } else if (s[i] == '(') {
+                st.push(sum);
+                st.push(sign);
+                sum = 0; sign = 1;
+            } else if (s[i] == ')') {
+                sign = st.top(); st.pop();
+                sum = st.top() + sign * sum;  st.pop();
+            }            
+        }
+        return sum;
+    }
+};
+
+// 2015.
 // Solution 1. Basic calculator, with "+-" and parentheses, no "*/"
 // Two stacks, one for number and one for operators (can use one, where operator can be stored by an integer sign)
 // 1. when seeing a digit, update number
