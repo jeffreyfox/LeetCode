37f558524f63de51a8db646f7bae524535ff6d66
jeffreyfox <jeffreyfox@users.noreply.github.com>
2021-12-20 16:13:34 -0800

    Create 322_coin-change.cc

---
+++ Algorithms/322_coin-change.cc
@@ -0,0 +1,53 @@
+/*
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+ 
+
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+*/
+
+// Solution using top-down DP with memoization.
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> mem(amount+1, 0);
+        return getCount(coins, amount, mem);
+    }
+    int getCount(vector<int>& coins, int amount, vector<int> &mem) {
+        if (amount < 0) return -1;
+        if (amount == 0) return 0;
+        if (mem[amount] != 0) return mem[amount];
+        int min_value = INT_MAX;
+        for (int coin : coins) {
+            if (coin > amount) continue;
+            int value = getCount(coins, amount - coin, mem);
+            if (value >= 0 && value < min_value) min_value = value;
+        }
+        mem[amount] = (min_value == INT_MAX) ? -1 : min_value + 1;
+        return mem[amount];
+    }
+};
