83a9e8bce32ac1e2b16147fd136c8e45c51eab40
jeffreyfox <jeffreyfox@users.noreply.github.com>
2021-12-27 14:41:14 -0800

    Create 476-number-complement.cc

---
+++ Algorithms/476-number-complement.cc
@@ -0,0 +1,41 @@
+/*
+The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+Given an integer num, return its complement.
+
+ 
+
+Example 1:
+
+Input: num = 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+Example 2:
+
+Input: num = 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+ 
+
+Constraints:
+
+1 <= num < 231
+*/
+
+// Find the most sigfinicant bit and generate a bit mask
+// e.g. 00000101 => 00001000 => 00000111
+// Beware of integer overflow! Use long long to prevent it.
+class Solution {
+public:
+    int findComplement(int num) {
+        int x = num;
+        long long y = 1;
+        while (x) {
+            x >>= 1;
+            y <<= 1;
+        }
+        y -= 1;
+        return ~num & y;
+    }
+};
