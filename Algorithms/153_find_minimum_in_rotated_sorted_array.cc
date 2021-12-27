6a1b073b287648f26f9cb9b713798334fd7dcfc2
jeffreyfox <jeffreyfox@users.noreply.github.com>
2021-12-27 09:17:24 -0800

    Update 153_find_minimum_in_rotated_sorted_array.cc

--- Algorithms/153_find_minimum_in_rotated_sorted_array.cc
+++ Algorithms/153_find_minimum_in_rotated_sorted_array.cc
@@ -33,8 +33,8 @@ Binary search. The array can be broken into two parts,
 for example, [4 5 6 7 0 1 2], first part is 4-7, second part is 0-2. If array is unrotated, then only first part exists (corner case). 
 
 There are three anchor points, lo, hi, and mid. Compare nums[mid] with nums[hi].
-1. If nums[mid] < nums[hi], then the minimum index must be <= mid, so set hi to mid (excluding nums[mid+1] and to the right)
-2. If nums[mid] > nums[hi], then mid must be in first part of the sorted array. This means that minimum index > mid, so set lo to mid+1.
+1. If nums[mid] < nums[hi], then it means the array is not rotated, and the minimum index must be <= mid, so set hi to mid (excluding nums[mid+1] and to the right)
+2. If nums[mid] > nums[hi], then it means the array is rorated, and mid is in first part of the sorted array. This means that minimum index > mid, so set lo to mid+1.
    Since nums[mid] > nums[hi], nums[mid] cannot be the minimum, so we can exlude it. 
 3. If nums[mid] = nums[hi], we can't say where mid is. It could be left or right. But we can safely exclude nums[hi], since it is a duplicated entry apart from nums[mid].
    Thus we simply decrement hi. 
@@ -44,12 +44,11 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         int lo = 0, hi = nums.size()-1;
-        int mid = 0;
         //invariant: [lo, hi] contains minimum
-        while(lo < hi) {
-            mid = lo + (hi-lo)/2;
-            if(nums[mid] < nums[hi]) hi = mid;
-            else if(nums[mid] > nums[hi]) lo = mid+1;
+        while (lo < hi) {
+            int mid = lo + (hi-lo)/2;
+            if (nums[mid] < nums[hi]) hi = mid;
+            else if (nums[mid] > nums[hi]) lo = mid+1;
             else hi--;
         }
         return nums[lo];
