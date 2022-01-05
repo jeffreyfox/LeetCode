13bc1e34831ca0249aea7e32979608e242df34e7
jeffreyfox <jeffreyfox@users.noreply.github.com>
2022-01-05 14:47:43 -0800

    Update 146_lru_cache.cc

--- Algorithms/146_lru_cache.cc
+++ Algorithms/146_lru_cache.cc
@@ -34,8 +34,8 @@ public:
     }
     
     int get(int key) {
-        auto iter = dict.find(key);
-        if (iter == dict.end()) {
+        auto iter = key_to_list.find(key);
+        if (iter == key_to_list.end()) {
             return -1;
         }
         DLLNode *node = iter->second;
@@ -45,8 +45,8 @@ public:
     }
     
     void put(int key, int value) {        
-        auto iter = dict.find(key);
-        if (iter != dict.end()) {
+        auto iter = key_to_list.find(key);
+        if (iter != key_to_list.end()) {
             // key already exists, updates value and move to the end of the list
             auto *node = iter->second;            
             node->value = value;            
@@ -57,12 +57,12 @@ public:
             // key does not exist, create a record in hashtable and add it to the end of the list
             DLLNode *new_node = new DLLNode(key, value);
             push_back(new_node);
-            dict[key] = new_node;
+            key_to_list[key] = new_node;
             N++;
             // If reaching capacity, remove the LRU element from cache and list
             if (N > maxN) {
                 DLLNode *old_node = head->next;
-                dict.erase(old_node->key);
+                key_to_list.erase(old_node->key);
                 remove(old_node);
                 delete old_node;
                 N--;
@@ -88,7 +88,7 @@ public:
 private:
     int maxN;  // capacity of the cache
     int N;  // number of keys in the cache
-    unordered_map<int, DLLNode*> dict;
+    unordered_map<int, DLLNode*> key_to_list;
     DLLNode *head;  // dummy head
 };
 
