a5ca82bc5deda820727b1ec817cdf8f06fdab4bc
jeffreyfox <jeffreyfox@users.noreply.github.com>
2022-05-25 21:03:42 -0700

    Update 056_merge_intervals.cc

--- Algorithms/056_merge_intervals.cc
+++ Algorithms/056_merge_intervals.cc
@@ -18,69 +18,35 @@ return [1,6],[8,10],[15,18].
 
 // One pass simple solution after sorting interval array
 
-bool compare_start(const Interval& a, const Interval& b) {
-    return a.start < b.start;
+bool compare(const vector<int> &a, const vector<int> &b) {
+    if (a[0] == b[0]) return a[1] < b[1];
+    return a[0] < b[0];
+}
+
+bool isOverlap(const vector<int> &a, const vector<int> &b) {
+    return a[0] <= b[1] && b[0] <= a[1];
+}
+
+vector<int> mergeOverlap(const vector<int> &a, const vector<int> &b) {
+    return {min(a[0], b[0]), max(a[1], b[1])};    
 }
 
 class Solution {
 public:
-    vector<Interval> merge(vector<Interval>& intervals) {
-        vector<Interval> result;
-        if(intervals.empty()) return result;
-        int n = intervals.size();
-        if(n == 1) return intervals;
-        sort(intervals.begin(), intervals.end(), compare_start);
-        result.push_back(intervals[0]);
-        Interval last = intervals[0];
-        for(int i = 1; i < intervals.size(); ++i) {
-            const Interval& curr = intervals[i];
-            if(curr.start <= last.end) {
-                //merge current with last
-                last.end = result.back().end = max(curr.end, result.back().end);
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> result;
+        if (intervals.empty()) return {};
+        std::sort(intervals.begin(), intervals.end(), compare);
+        vector<int> curr = intervals[0];
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (isOverlap(intervals[i], curr)) {
+                curr = mergeOverlap(intervals[i], curr);
             } else {
                 result.push_back(curr);
-                last = curr;
+                curr = intervals[i];
             }
         }
+        result.push_back(curr);
         return result;
-    }
-};
-
-
-// An old solution 
-bool compare(const Interval &a, const Interval &b) {
-    if(a.start < b.start) return true;
-    if(a.start > b.start) return false;
-    return a.end < b.end;
-}
-
-class Solution {
-public:
-
-    vector<Interval> merge(vector<Interval>& intervals) {
-        sort(intervals.begin(), intervals.end(), compare);
-        vector<Interval> s; //stack 
-        int n = intervals.size();
-        for(int i = 0; i < n; ++i) {
-            Interval it = intervals[i];
-            while(!s.empty() && isOverlap(s.back(), it)) { //overlap
-                it = mergeTwo(s.back(), it);
-                s.erase(s.end()-1); //stack pop
-            } 
-            //now either stack is empty, or there is no overlap
-            s.insert(s.end(), it); //stack push
-        }
-        return s; //return whole stack
-    }
-    
-    bool isOverlap(const Interval& a, const Interval &b) {
-        return (a.start <= b.end && b.start <= a.end);
-    }
-    
-    Interval mergeTwo(const Interval &a, const Interval &b) {
-        Interval it;
-        it.start = min(a.start, b.start);
-        it.end = max(a.end, b.end);
-        return it;
     }
 };
