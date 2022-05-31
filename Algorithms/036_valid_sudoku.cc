85f1cffcd3643f00198b9823167e0d28b6008334
jeffreyfox <jeffreyfox@users.noreply.github.com>
2022-05-31 09:22:22 -0700

    Update 036_valid_sudoku.cc

--- Algorithms/036_valid_sudoku.cc
+++ Algorithms/036_valid_sudoku.cc
@@ -9,6 +9,48 @@ Note:
 A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated. 
 */
 
+// Vanilla solution using a state vector.
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<bool> states(9, false);
+        
+        // check rows
+        for (int ir = 0; ir < 9; ++ir) {
+            std::fill(states.begin(), states.end(), false);
+            for (int jc = 0; jc < 9; ++jc) {
+                char c = board[ir][jc];
+                if (c == '.') continue;
+                if (states[c-'1']) return false;
+                states[c-'1'] = true;
+            }
+        }
+
+        // check columns
+        for (int jc = 0; jc < 9; ++jc) {
+            std::fill(states.begin(), states.end(), false);
+            for (int ir = 0; ir < 9; ++ir) {
+                char c = board[ir][jc];
+                if (c == '.') continue;
+                if (states[c-'1']) return false;
+                states[c-'1'] = true;
+            }
+        }
+
+        // check blocks
+        for (int ib = 0; ib < 9; ++ib) {
+            std::fill(states.begin(), states.end(), false);
+            for (int jb = 0; jb < 9; ++jb) {
+                char c = board[3*(ib/3)+jb/3][3*(ib%3)+jb%3];
+                if (c == '.') continue;
+                if (states[c-'1']) return false;
+                states[c-'1'] = true;
+            }
+        }
+        return true;
+    }
+};
+
 /// Check each row, column and block, record the state of each check.
 /// Can use a single integer to record the state, using bit operations
 ///  1  1  1  1  1  1  1  1  1
