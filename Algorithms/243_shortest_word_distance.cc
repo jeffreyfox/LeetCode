15adbdb819c1a5decbb77f4f29ca3eb10a29e394
jeffreyfox <jeffreyfox@users.noreply.github.com>
2022-06-16 14:27:54 -0700

    Update 243_shortest_word_distance.cc

--- Algorithms/243_shortest_word_distance.cc
+++ Algorithms/243_shortest_word_distance.cc
@@ -1,51 +1,20 @@
-/*
-Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
-
-For example,
-Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
-
-Given word1 = “coding”, word2 = “practice”, return 3.
-Given word1 = "makes", word2 = "coding", return 1.
-
-Note:
-You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
-*/
-
-// Solution 1. Keep two indices, one for each word (20ms)
-// optimizations: do not use std::min() function, also prestore the word.size() as n
-
+// One pass solution. Update the indices of the two words and compute the new distance.
+// Use a variable to track the minimum distance seen so far.
 class Solution {
 public:
-    int shortestDistance(vector<string>& words, string word1, string word2) {
-        int j1(-1), j2(-1), mind = INT_MAX, n = words.size();
-        for(int i = 0; i < n; i++) {
-            const string& w = words[i];
-            if(w == word1) {
-                j1 = i;
-                if(j2 >= 0 && mind > j1-j2) mind = j1-j2;
-            } else if(w == word2) {
-                j2 = i;
-                if(j1 >= 0 && mind > j2-j1) mind = j2-j1;
+    int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
+        int idx1 = -1, idx2 = -1;
+        int min_dist = INT_MAX;
+        for (int i = 0; i < wordsDict.size(); ++i) {
+            if (wordsDict[i] == word1) {
+                idx1 = i;
+                if (idx2 >= 0) min_dist = min(min_dist, idx1 - idx2);
+            }
+            else if (wordsDict[i] == word2) {
+                idx2 = i;
+                if (idx1 >= 0) min_dist = min(min_dist, idx2 - idx1);
             }
         }
-        return mind;
+        return min_dist;
     }
 };
-
-// Solution 2. Use just one index, before assigning the new index, check the difference with previous, also need to check the current word is the same as last word
-class Solution {
-public:
-    int shortestDistance(vector<string>& words, string word1, string word2) {
-        int index(-1), mind = INT_MAX, n = words.size();
-        for(int i = 0; i < n; i++) {
-            if(words[i] == word1 || words[i] == word2) {
-                if(index != -1 && words[index] != words[i]) { //not the same word
-                    mind = min(mind, i-index);
-                }
-                index = i;
-            }
-        }
-        return mind;
-    }
-};
-
