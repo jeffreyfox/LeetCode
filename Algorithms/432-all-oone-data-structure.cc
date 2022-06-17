e98e0b121e21f095669c3c5d4090e032c2ae8372
jeffreyfox <jeffreyfox@users.noreply.github.com>
2022-06-17 08:50:45 -0700

    Create 432-all-oone-data-structure.cc

---
+++ Algorithms/432-all-oone-data-structure.cc
@@ -0,0 +1,86 @@
+// Use a doubly linked list and a hash map.
+// Use a dummy head, which complicates the process of finding the new insertion point.
+
+class AllOne {
+public:
+    struct DllNode {
+        string key;
+        int val;
+        DllNode *prev, *next;
+        DllNode(const string& k, int v) : key(k), val(v), prev(nullptr), next(nullptr) {}
+    };
+
+    AllOne() {
+        head = new DllNode("", -1);
+        head->next = head;
+        head->prev = head;
+    }
+    
+    void inc(string key) {
+        if (!dict.count(key)) {
+            DllNode *node = new DllNode(key, 1);
+            dict[key] = node;
+            // insert after head
+            insert(head, node);            
+        } else {
+            DllNode *node = dict[key];
+            node->val++;
+            DllNode *p = node;
+            while (p->next != head && p->next->val < node->val) p = p->next;
+            if (p != node) {
+                // insert after p
+                remove(node);
+                insert(p, node);
+            }
+        }        
+    }
+    
+    void dec(string key) {
+        DllNode* node = dict[key];
+        --node->val;
+        if (node->val == 0) {
+            dict.erase(key);
+            remove(node);
+        } else {
+            // find the previous node to link after
+            DllNode *p = node;
+            while (p->prev != head && p->prev->val > node->val) p = p->prev;
+            if (p != node) {
+                // insert after p->prev
+                remove(node);
+                insert(p->prev, node);
+            }
+        }
+    }
+    
+    string getMaxKey() {
+        return head->prev->key;
+    }
+    
+    string getMinKey() {
+        return head->next->key;
+    }
+private:
+    void insert(DllNode* prev, DllNode *node) {        
+        node->next = prev->next;
+        node->prev = prev;        
+        prev->next->prev = node;
+        prev->next = node;
+    }
+    void remove(DllNode *node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    DllNode *head;
+    unordered_map<string, DllNode*> dict;
+};
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne* obj = new AllOne();
+ * obj->inc(key);
+ * obj->dec(key);
+ * string param_3 = obj->getMaxKey();
+ * string param_4 = obj->getMinKey();
+ */
