824581dc5ba605543a8c2e8cf378256f2db935fb
jeffreyfox <jeffreyfox@users.noreply.github.com>
2022-06-17 13:49:30 -0700

    Update 146_lru_cache.cc

--- Algorithms/146_lru_cache.cc
+++ Algorithms/146_lru_cache.cc
@@ -92,149 +92,57 @@ private:
     DLLNode *head;  // dummy head
 };
 
+// Same idea using std::list.
+// Use list::splice to move an element from middle of the list to the end of the list.
+class LRUCache {
+public:
+    struct Record {
+        int key;
+        int value;
+        Record(int k, int v) : key(k), value(v) {}
+    };
+
+    LRUCache(int capacity) {
+        cap = capacity;
+        count = 0;
+    }
+    
+    int get(int key) {
+        if (!dict.count(key)) return -1;
+        auto curr = dict[key];
+        // Move to the end of the list
+        dll.splice(dll.end(), dll, curr);
+        return curr->value;
+    }
+    
+    void put(int key, int value) {
+        if (dict.count(key)) {
+            auto curr = dict[key];
+            curr->value = value;
+            dll.splice(dll.end(), dll, curr);
+        } else {
+            // new element, insert to tail
+            dll.push_back(Record(key, value));
+            dict[key] = std::next(dll.end(), -1);
+            count ++;
+        }
+        if (count > cap) {
+            dict.erase(dll.begin()->key);
+            dll.pop_front();
+            count--;
+        }
+    }
+    
+private:
+    int cap;
+    int count;
+    list<Record> dll;
+    unordered_map<int, list<Record>::iterator> dict;    
+};
+
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
-
-// Same idea using std::list.
-// Use std::prev to point to the last element.
-// std::list does not expose a way to move an element to the end. So we first delete and then push back a new one. This means we need to update the hashtable (whereas in the
-// previous implementation the same node is moved to the end of list.
-
-class LRUCache {
-public:
-    // Uses a double-linked list for O(1) deletion
-    struct Record {
-        int key;
-        int value;
-        Record(int k, int v) : key(k), value(v) {}
-    };
-    
-    LRUCache(int capacity) {
-        N = 0;
-        maxN = capacity;        
-    }
-    
-    int get(int key) {
-        auto iter = dict.find(key);
-        if (iter == dict.end()) {
-            return -1;
-        }
-        auto list_iterator = iter->second;
-        auto record = *list_iterator;
-        records.erase(list_iterator);
-        records.push_back(record);
-        dict[key] = prev(records.end(), 1);
-        return record.value;
-    }
-    
-    void put(int key, int value) {        
-        auto iter = dict.find(key);
-        if (iter != dict.end()) {
-            // key already exists, updates value and move to the end of the list
-            auto list_iterator = iter->second;
-            list_iterator->value = value;   
-            // Move the node to the end of the list
-            auto record = *list_iterator;
-            records.erase(list_iterator);
-            records.push_back(record);
-            dict[key] = prev(records.end(), 1);
-        } else {
-            // key does not exist, create a record in hashtable and add it to the end of the list
-            records.push_back(Record(key, value));
-            dict[key] = prev(records.end(), 1);
-            N++;
-            // If reaching capacity, remove the LRU element from cache and list
-            if (N > maxN) {                
-                dict.erase(records.begin()->key);
-                records.erase(records.begin());
-                N--;
-            }
-        }
-    }
-
-private:
-    int maxN;  // capacity of the cache
-    int N;  // number of keys in the cache
-    unordered_map<int, list<Record>::iterator> dict;
-    list<Record> records;
-};
-
-class LRUCache{
-
-public:
-    struct ListNode {
-        ListNode *prev, *next;
-        int key, val;
-        ListNode(int k, int v) : key(k), val(v), prev(NULL), next(NULL) {}
-    };
-
-    LRUCache(int capacity) {
-        this->capacity = capacity;
-        this->N = 0;
-        //dummy head and tail
-        head = new ListNode(-1, 0);
-        tail = new ListNode(-1, 0);
-        head->next = tail;
-        tail->prev = head;
-    }
-
-    ~LRUCache() {
-        ListNode *node = head;
-        while(node) {
-            ListNode* tmp = node;
-            node = node->next;
-            delete tmp;
-        }
-    }
-
-    int get(int key) {
-        if(dict.count(key)) {
-            ListNode* node = dict[key];
-            removeFromList(node);
-            appendToList(node);
-            return node->val;
-        } else return -1;       
-    }
-    
-    void set(int key, int value) {
-        if(dict.count(key)) { //found, just update
-            ListNode* node = dict[key];
-            node->val = value;
-            removeFromList(node);
-            appendToList(node);
-        } else { //not found, insert
-            ListNode *node = new ListNode(key, value);
-            dict[key] = node;
-            appendToList(node);
-            N++;
-            if(N > capacity) {
-               node = head->next;
-               removeFromList(node);
-               dict.erase(node->key); //erase entry from map
-               delete node;
-               N--;
-            }
-        }
-    }
-
-private:
-    void removeFromList(ListNode* node) {
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
-    }
-    void appendToList(ListNode* node) {
-        node->prev = tail->prev;
-        node->next = tail;
-        tail->prev->next = node;
-        tail->prev = node;
-    }
-    int capacity;
-    int N;
-    unordered_map<int, ListNode*> dict;
-    ListNode *head, *tail;
-};
-
