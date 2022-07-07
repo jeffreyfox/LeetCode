be04636e7ed905a498614223e658cfb7a67ba882
jeffreyfox <jeffreyfox@users.noreply.github.com>
2022-07-07 14:59:21 -0700

    Update 003_longest_substr_wo_repeating_char.cc

--- Algorithms/003_longest_substr_wo_repeating_char.cc
+++ Algorithms/003_longest_substr_wo_repeating_char.cc
@@ -1,8 +1,5 @@
-/*
-Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
-*/
-
-// Solution 1, use a map to store the position of each character (since s is string, can use an indexed array which is faster than map). Scan from left to right, and maintain a map to store characters already seen and its last position, update length each time.
+// Solution 1, use a map to store the position of each character (since s is string, can use an indexed array which is faster than map). Scan from left to right,
+// and maintain a map to store characters already seen and its last position, update length each time.
 // Use a pointer (i) to store the current front of substring
 // When a character is not in the map, insert it to map; 
 // When a character is found and its index is within i and j, then move i to one character to the right of it. 
