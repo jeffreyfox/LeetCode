d620c5186ceb21565e7dc71342a10ddc0c34b81c
jeffreyfox <jeffreyfox@users.noreply.github.com>
2022-07-18 11:06:23 -0700

    Update 1188-design-bounded-blocking-queue.cc

--- Algorithms/1188-design-bounded-blocking-queue.cc
+++ Algorithms/1188-design-bounded-blocking-queue.cc
@@ -1,36 +1,70 @@
-class BoundedBlockingQueue {
-private:
-    int N;
-    int sz;
-    mutex m;
-    condition_variable cv;
-    deque<int> arr;
+// Solution using a single condition variable. notify_all() is used.
+// This is not optimized as a consumer thread may wake up other consumer threads (similarly for producer) causing spurious wake ups.
+// A better solution is to use two CVs and let consumer only wake up producer and vice versa.
 
+class BoundedBlockingQueue {
 public:
     BoundedBlockingQueue(int capacity) {
-        N = capacity;
-        sz = 0;
+        this->capacity = capacity;
     }
     
     void enqueue(int element) {
-        unique_lock<mutex> lock(m);
-        cv.wait(lock, [this]{return sz < N;});
-        arr.push_front(element);
-        sz++;
+        unique_lock<mutex> ul(m);
+        cv.wait(ul, [this](){ return q.size() < capacity;});
+        q.push_back(element);
         cv.notify_all();
     }
     
     int dequeue() {
-        unique_lock<mutex> lock(m);
-        cv.wait(lock, [this]{return sz > 0;});
-        int result = arr.back();
-        arr.pop_back();
-        sz--;
+        unique_lock<mutex> ul(m);
+        cv.wait(ul, [this](){ return q.size() > 0;});
+        int v = q.front();
+        q.pop_front();
         cv.notify_all();
-        return result;
+        return v;
     }
     
     int size() {
-        return sz;
+        unique_lock<mutex> ul(m);
+        return q.size();
     }
+private:
+    int capacity;
+    deque<int> q;
+    mutex m;
+    condition_variable cv;
+};
+
+// An improved solution using two condition variables.
+class BoundedBlockingQueue {
+public:
+    BoundedBlockingQueue(int capacity) {
+        this->capacity = capacity;
+    }
+    
+    void enqueue(int element) {
+        unique_lock<mutex> ul(m);
+        cv_full.wait(ul, [this](){ return q.size() < capacity;});
+        q.push_back(element);
+        cv_empty.notify_one();
+    }
+    
+    int dequeue() {
+        unique_lock<mutex> ul(m);
+        cv_empty.wait(ul, [this](){ return q.size() > 0;});
+        int v = q.front();
+        q.pop_front();
+        cv_full.notify_one();
+        return v;
+    }
+    
+    int size() {
+        unique_lock<mutex> ul(m);
+        return q.size();
+    }
+private:
+    int capacity;
+    deque<int> q;
+    mutex m;
+    condition_variable cv_full, cv_empty;
 };
