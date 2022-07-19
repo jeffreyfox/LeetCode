340af00085c6975f71f12fd5f074365499ca51eb
jeffreyfox <jeffreyfox@users.noreply.github.com>
2022-07-19 12:08:21 -0700

    Update 127_word_ladder.cc

--- Algorithms/127_word_ladder.cc
+++ Algorithms/127_word_ladder.cc
@@ -24,88 +24,53 @@ Note:
 
 /* 
 === Solution 1 ===
-One-way BFS from beinWord to endWord. Do not remove elements from the original dictionary! Rather create a new one (unvisited) and remove from it.
-Systematically trying the new word by altering one character of the new word. Do not create a new string, rather change the character in-place and revert back to original afterwards.
+One-way BFS from beinWord to endWord. Create a new dict to track words already visited.
+BFS with two sets, one for current level, one for next level, and in the end of each iteration update accordingly
+Systematically trying the new word by altering one character of the new word. Do not create a new string, rather change the character in-place and
+revert back to original afterwards.
 */
 
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
-       deque<pair<string, int> > q;
-       unordered_set<string> unvisited = wordList;
-       int wl = beginWord.size();
-
-       q.push_back(make_pair(beginWord, 1));
-       unvisited.erase(beginWord); //remove beginWord to unvisited
-       unvisited.insert(endWord); //insert beginWord to unvisited
-       while(!q.empty()) {
-           string wd = q.front().first;
-           int level = q.front().second;
-           if(wd == endWord) return level;
-           q.pop_front();
-           for(int i = 0; i < wl; ++i) {
-               char c = wd[i];
-               for(char ch = 'a'; ch <= 'z'; ++ch) {
-                   if(ch == c) continue;
-                   wd[i] = ch;
-                   if(unvisited.count(wd))  {
-                       q.push_back(make_pair(wd, level+1));
-                       unvisited.erase(wd);
-                   }
-               }
-               wd[i] = c; //revert back
-           }
-       }
-
-       return 0;
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict;
+        for (const auto &w : wordList) dict.insert(w);
+        
+        int counter = 1;
+        unordered_set<string> seen;
+        vector<string> curr, next;
+        curr.push_back(beginWord);     
+        
+        while (!curr.empty()) {
+            for (const auto &word : curr) {
+                // try all combinations
+                string newWord = word;
+                for (int i = 0; i < newWord.size(); ++i) {
+                    char tmp = newWord[i];
+                    for (char c = 'a'; c <= 'z'; ++c) {
+                        if (c == tmp) continue;
+                        newWord[i] = c;                        
+                        if (!dict.count(newWord)) continue;
+                        if (seen.count(newWord)) continue;
+                        
+                        if (newWord == endWord) return counter+1;
+                        
+                        seen.insert(newWord);
+                        next.push_back(newWord);
+                    }
+                    newWord[i] = tmp;
+                }
+            }
+            curr = next;
+            next.clear();
+            counter++;
+        }
+        return 0;
     }
 };
 
 /*
 === Solution 2 ===
-BFS with two sets, one for current level, one for next level, and in the end of each iteration swap the two.
-*/
-
-class Solution {
-public:
-    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
-       vector<string> current, next;
-       int level = 1;
-       unordered_set<string> unvisited = wordList;
-       int wl = beginWord.size();
-
-       unvisited.erase(beginWord);
-       unvisited.insert(endWord);
-
-       current.push_back(beginWord);
-       
-       while(!current.empty()) {
-           for(vector<string>::iterator it = current.begin(); it != current.end(); ++it) {
-               string wd = *it;
-               for(int i = 0; i < wl; ++i) {
-                   char c = wd[i];
-                   for(char ch = 'a'; ch <= 'z'; ++ch) {
-                       if(ch == c) continue;
-                       wd[i] = ch;
-                       if(wd == endWord) return level+1;
-                       if(unvisited.count(wd))  {
-                           next.push_back(wd);
-                           unvisited.erase(wd);
-                       }
-                   }
-                   wd[i] = c; //revert back
-               }
-           }
-           current.swap(next);
-           next.clear();
-           level++;
-       }
-       return 0;        
-    }
-};
-
-/*
-=== Solution 3 ===
 Bi-directional BFS to reduce search space. Start from two ends (begin/end), and only do BFS from the side with smaller search space. 
 When we find one string in the search front that belongs to the front of the other side, we have "connected" the two sides.
 */
